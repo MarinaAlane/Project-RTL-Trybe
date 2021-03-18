6682f330861e34649950cff26e55e1bcbcdc72aa
Diogenes <diogenesfelipe.s@outlook.com>
2021-03-18 19:03:50 -0300

    fix/Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -8,7 +8,7 @@ import App from '../App';
 describe('Teste do componente FavoritePokemons', () => {
   it('Teste se é exibido na tela a mensagem "No favorite pokemon found", inicial', () => {
     renderWithRouter(<FavoritePokemons />);
-    expect(screen.getByText(/No favorite pokemon found/)).toBeInTheDocument();
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
   });
 
   it('Teste se é exibido todos os cards de pokémons favoritados', () => {
@@ -19,5 +19,15 @@ describe('Teste do componente FavoritePokemons', () => {
     userEvent.click(checkboxFavorite);
     userEvent.click(screen.getByText('Favorite Pokémons'));
     expect(screen.getByText('Pikachu')).toBeInTheDocument();
+  });
+
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado', () => {
+    renderWithRouter(<App />);
+    const linkDetails = screen.getByText('More details');
+    userEvent.click(linkDetails);
+    const checkboxFavorite = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(checkboxFavorite);
+    userEvent.click(screen.getByText('Favorite Pokémons'));
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
   });
 });
