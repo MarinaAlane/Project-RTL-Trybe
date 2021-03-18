eeb0527d4b1b23927b4c8f593c411ce33d4d750f
Lucas Silva <martssilvapro@gmail.com>
2021-03-18 16:37:20 -0300

    adicionado renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -22,3 +22,49 @@ test('shows the Pokédex when the route is `/`', () => {
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
+
+describe('Se topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  it('O primeiro link deve possuir o texto Home', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const homeLink = getByText('Home');
+
+    expect(homeLink).toBeInTheDocument();
+  });
+
+  it('O segundo link deve possuir o texto About', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const aboutLink = getByText('About');
+
+    expect(aboutLink).toBeInTheDocument();
+  });
+
+  it('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const favoritePokemonsLink = getByText('Favorite Pokémons');
+
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
+});
+
+test('Teste se a aplicação é redirecionada para a página inicial', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+});
