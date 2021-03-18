c084004f0795e0698310d7ccf2218edd4e864375
johnealves <johne_af@hotmail.com>
2021-03-18 19:43:37 -0300

    commit

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,70 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+
+describe('Teste o componente Pokedex', () => {
+  const isPokemonFavoriteById = {
+    4: false,
+    10: false,
+    23: false,
+    25: true,
+    65: false,
+    78: false,
+    143: false,
+    148: false,
+    151: false,
+  };
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />);
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: 'Encountered pokémons',
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se próximo Pokémon da lista aparece ao clicar no botão', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />);
+
+    const btn = screen.getByText(/Próximo pokémon/i);
+    let counter = 0;
+    pokemons.forEach(() => {
+      if (counter === pokemons.length) {
+        counter = 0;
+      }
+      const name = screen.getByText(pokemons[counter].name);
+      expect(name).toBeInTheDocument();
+      userEvent.click(btn);
+      counter += 1;
+    });
+  });
+
+  it('Teste se é mostrado apenas um Pokémon por vez.', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />);
+    const allPokemons = screen.getAllByTestId(/pokemon-name/);
+    expect(allPokemons.length).toBe(1);
+  });
+
+  it('Teste se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ isPokemonFavoriteById }
+    />);
+    const allBtn = screen.getAllByTestId('pokemon-type-button');
+    allBtn.forEach((btn) => {
+      console.log(btn.value);
+    });
+  });
+});
