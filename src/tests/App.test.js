f9083167bceea161a0143e84c38e9ddd5cab9b14
Felipe Cardoso <felipedclc@gmail.com>
2021-03-18 16:02:51 -0300

    iniciando o projeto react-testing-library.  VAMOS COM TUDO!

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../RenderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +12,35 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+describe('checking that the routes are correct', () => {
+  it('route to home', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const home = getByText('Encountered pokémons');
+    expect(home).toBeInTheDocument();
+    fireEvent.click(getByText('Home'));
+    expect(home).toBeInTheDocument();
+  });
+
+  it('route to about', () => {
+    const { getByText, getByAltText } = renderWithRouter(<App />);
+
+    const about = getByText('About');
+    expect(about).toBeInTheDocument();
+    fireEvent.click(about);
+    const aboutPage = getByAltText('Pokédex');
+    expect(aboutPage).toBeInTheDocument();
+  });
+
+  it('route to favorite pokemons', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const favoritePokemons = getByText('Favorite Pokémons');
+    expect(favoritePokemons).toBeInTheDocument();
+    fireEvent.click(favoritePokemons);
+    const aboutPage = getByText('Favorite pokémons');
+    expect(aboutPage).toBeInTheDocument();
+  });
 });
