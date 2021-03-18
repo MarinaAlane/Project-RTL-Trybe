d9da5a3859fd3c831f52e6e188a133e0afed6491
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-03-18 17:46:14 -0300

    requirement 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,39 @@
-test('', () => {});
+// import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
+// import FavoritePokemons from '../components/FavoritePokemons';
+
+// describe('Component <FavoritePokemons.js /> Test', () => {
+//   test('renders a not found message', () => {
+//     const { getByText } = render(
+//       <MemoryRouter>
+//         <FavoritePokemons />
+//       </MemoryRouter>,
+//     );
+
+//     const heading = getByText(/No favorite pokemon found/i);
+//     expect(heading).toBeInTheDocument();
+//   });
+
+//   test('show favorite cards', () => {
+//     const { getByText } = render(
+//       <MemoryRouter>
+//         <FavoritePokemons />
+//       </MemoryRouter>,
+//     );
+
+//     const heading = getByText(/No favorite pokemon found/i);
+//     expect(heading).toBeInTheDocument();
+//   });
+
+//   test('show no cards if no favorites', () => {
+//     const { getByText } = render(
+//       <MemoryRouter>
+//         <FavoritePokemons />
+//       </MemoryRouter>,
+//     );
+
+//     const heading = getByText(/No favorite pokemon found/i);
+//     expect(heading).toBeInTheDocument();
+//   });
+// });
