d4593d7645c5ab8af9fc7fac9aaf293ae5f9d216
MuriloBatista <mub4si@gmail.com>
2021-03-18 16:29:41 -0300

    Initial commit and Req:01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
+test('Renderiza um titulo contendo a palavra `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -11,4 +12,46 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('Se tem 3 Links com os nomes `Home`, `About` e `Favorite Pokémons`', () => {
+  const { getByText } = renderWithRouter(<App />);
+
+  const home = getByText('Home');
+  const about = getByText('About');
+  const favorite = getByText('Favorite Pokémons');
+
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favorite).toBeInTheDocument();
+});
+
+test('Se os links redirecionam as páginas corretas', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  // Home
+  fireEvent.click(getByText('Home'));
+  const pathHome = history.location.pathname;
+  const home = getByText('Encountered pokémons');
+  expect(pathHome).toBe('/');
+  expect(home).toBeInTheDocument();
+
+  // About
+  fireEvent.click(getByText('About'));
+  const pathAbout = history.location.pathname;
+  const about = getByText('About Pokédex');
+  expect(pathAbout).toBe('/about');
+  expect(about).toBeInTheDocument();
+
+  // Favorite Pokémons
+  fireEvent.click(getByText('Favorite Pokémons'));
+  const pathFavorite = history.location.pathname;
+  const favorite = getByText('Favorite pokémons');
+  expect(pathFavorite).toBe('/favorites');
+  expect(favorite).toBeInTheDocument();
+
+  // NotFound
+  history.push('/paginanãoexiste');
+  const notFound = getByText('Page requested not found');
+  expect(notFound).toBeInTheDocument();
 });
