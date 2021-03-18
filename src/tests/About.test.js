4a191f126a12439ae59ef98d6aad0c1670b5b447
Luciano_Lodi <lrlodi@gmail.com>
2021-03-18 17:46:19 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render, fireEvent } from '@testing-library/react';
-import renderWithRouter from './renderWithRouter';
+import { render } from '@testing-library/react';
 import About from '../components/About';
 
 describe('Requisito 2', () => {
   test('tests if page contains information about Pokedex', () => {
-    const { getByText } = render(<About />);
-    const paragraph1 = `This application simulates a Pokédex,
-      a digital encyclopedia containing all Pokémons`;
-    const paragraph2 = `One can filter Pokémons by type,
-    and see more details for each one of them`;
+    render(<About />);
+    const paragraph1 = 'This application simulates a Pokédex'
+      + ', a digital encyclopedia containing all Pokémons';
+    const paragraph2 = 'One can filter Pokémons by type'
+      + ', and see more details for each one of them';
     const paragraphs = document.querySelectorAll('p');
     expect(paragraphs[0].textContent).toBe(paragraph1);
     expect(paragraphs[1].textContent).toBe(paragraph2);
@@ -20,5 +18,13 @@ describe('Requisito 2', () => {
     const { getByRole } = render(<About />);
     const title = getByRole('heading', { level: 2 });
     expect(title.textContent).toBe('About Pokédex');
+  });
+
+  test('tests if there is an image of a Pokédex', () => {
+    render(<About />);
+    const imgAltText = 'https://cdn.bulbagarden.net/upload/'
+      + 'thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const renderedImg = document.querySelector('img');
+    expect(renderedImg.src).toBe(imgAltText);
   });
 });
