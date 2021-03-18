46c00ee91bb664f159c03d3255bed75b5ae05b92
Lucas Silva <martssilvapro@gmail.com>
2021-03-18 17:35:46 -0300

    requisito 1 feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -61,10 +62,57 @@ describe('Se topo da aplicação contém um conjunto fixo de links de navegaçã
   });
 });
 
-test('Teste se a aplicação é redirecionada para a página inicial', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+describe('Testando os links do header', () => {
+  it('Teste se a aplicação é redirecionada para a página inicial na URL /', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText(/Home/i));
+
+    const { location } = history;
+
+    const { pathname } = location;
+
+    expect(pathname).toBe('/');
+
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de About, na URL /about', () => {
+    const { getByText, getByAltText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText(/About/i));
+
+    const { location } = history;
+
+    const { pathname } = location;
+
+    expect(pathname).toBe('/about');
+
+    const img = getByAltText('Pokédex');
+    expect(img).toBeInTheDocument();
+  });
+
+  it('Testando o link Favorite Pokémon', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+
+    const { location } = history;
+
+    const { pathname } = location;
+
+    expect(pathname).toBe('/favorites');
+
+    const favoriteText = getByText('Favorite pokémons');
+    expect(favoriteText).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página Not Found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/testarrghineedalcohol');
+    const notFound = getByText(/Page requested not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
 });
