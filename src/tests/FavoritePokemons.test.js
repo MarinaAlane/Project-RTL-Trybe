85ed84537982646bc557feb3610f1c1bb9a10f63
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-03-18 14:47:47 -0300

    commit inicial

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import App from '../App';
+
+afterEach(cleanup);
+
+// arquivo App.test.js pode servir de exemplo
+describe('Teste da aplicação, testando o botão e sua funcionalidade', () => {
+  test('ttt', () => {});
+});
