3b58b71034b8858a2da567fcb5bc0d68a200f9d4
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-03-18 18:29:59 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const paragraphs = getByText(/No favorite pokemon found/i);
+  expect(paragraphs).toBeInTheDocument();
+});
+
+test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+  const { getByRole, getByText, getByTestId } = renderWithRouter(<App />);
+  const moreDetails = getByText('More details');
+  fireEvent.click(moreDetails);
+  const checkBox = getByRole('checkbox');
+  fireEvent.click(checkBox);
+  const favoritePokemons = getByText('Favorite Pokémons');
+  fireEvent.click(favoritePokemons);
+  const favPokemons = getByTestId('fav-pokemon');
+  expect(favPokemons.children.length).toBe(1);
+});
+
 test('', () => {});
