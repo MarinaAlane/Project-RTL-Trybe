008fd21617573ceb70645576f6b03da027a96e5e
Rafael Pufal <rafael.pufal23@gmail.com>
2021-03-19 15:18:44 -0300

    requisito 5

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('tests regarding Pokemon.js component', () => {
+  it('checks if correct structure is displayed', () => {
+    const { getByRole, getByTestId } = renderWithRouter(<App />);
+    fireEvent.click(getByRole('button', { name: 'Dragon' }));
+    expect(getByTestId('pokemon-name')).toHaveTextContent('Dragonair');
+  });
+});
