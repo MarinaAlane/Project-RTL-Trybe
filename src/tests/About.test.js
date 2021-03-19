9db69b037f5563d8f3b8e3469a4b96cb3d8ea71d
MuriloBatista <mub4si@gmail.com>
2021-03-19 11:19:05 -0300

    Req:04

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,8 +4,11 @@ import { About } from '../components';
 
 describe('Testa o comportamento do Component <About />', () => {
   test('Se a página contém um header com o texto `About Pokédex`', () => {
-    const { getByText } = render(<About />);
-    const header = getByText('About Pokédex');
+    const { getByRole } = render(<About />);
+    const header = getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/,
+    });
     expect(header).toBeInTheDocument();
   });
 
@@ -18,7 +21,7 @@ describe('Testa o comportamento do Component <About />', () => {
   test('Se a URL da imagem renderizada está correta.', () => {
     const { getByRole } = render(<About />);
     const image = getByRole('img');
-
+    console.log(image);
     expect(image.src)
       .toBe('https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa o comportamento do Component NotFound', () => {
+  test('Se contem um elemento h2 com o texto `Page requested not found`', () => {
+    const { getByRole } = render(<NotFound />);
+    const header = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/,
+    });
+
+    expect(header).toBeInTheDocument();
+  });
+
+  test('Se a URL da imagem renderizada está correta', () => {
+    const { getAllByRole } = render(<NotFound />);
+    const image = getAllByRole('img');
+
+    expect(image[1].src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
