d67d02d0ed65357d47f08c671b2b106b9bb3318a
edison-ps <edison@osite.com.br>
2021-03-19 19:09:53 -0300

    Requisito 2 V1.00

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,4 @@
 import React from 'react';
-// import { MemoryRouter } from 'react-router-dom';
-// import { render, screen } from '@testing-library/react';
-// import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
