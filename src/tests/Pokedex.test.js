42d45253db28bfeda613d2e4cd4e72309b7bb452
BernardoFontes <bfontes.1997@gmail.com>
2021-03-19 17:58:18 -0300

    commitando requisito pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,65 +1,74 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderpag from '../services/renderpag';
-
 import pokemons from '../data';
 
-describe('Teste do componente Pokedéx', () => {
-  test('Parte 1', () => {
-    const { getByTestId, getAllByRole } = renderpag(<App />);
-    const data = pokemons[0];
-
-    const nome = getByTestId('pokemon-name');
-    expect(nome.textContent).toBe(data.name);
-    const tipo = getByTestId('pokemonType');
-    expect(tipo.textContent).toBe(data.type);
-    const peso = getByTestId('pokemon-weight');
-    expect(peso.textContent).toBe(`Average weight: ${aveWeight} ${unit}`);
-    const img = getAllByRole('img')[0];
-    expect(img).toHaveAttribute('src', data.image);
-    expect(img).toHaveAttribute('alt', `${data.name} sprite`);
+describe('Testes requisito Pokedex', () => {
+  test('testa os tipos de pokemon',
+    () => {
+      const tipos = [
+        'Electric',
+        'Fire',
+        'Bug',
+        'Poison',
+        'Psychic',
+        'Normal',
+        'Dragon',
+      ];
+      const { queryAllByTestId } = renderpag(<App />);
+      const botaoTipos = queryAllByTestId('pokemon-type-button');
+      expect(botaoTipos.length).toBe(tipos.length);
+      expect(botaoTipos[0].textContent).toBe(tipos[0]);
+    });
+  test('header', () => {
+    const { getByRole } = renderpag(<App />);
+    const header = getByRole('heading', { level: 2, name: /encountered pokémons/i });
+    expect(header).toBeInTheDocument();
   });
+  test('testando botão que traz o próximo pokemon', () => {
+    const { getByRole, getByText } = renderpag(<App />);
+    const botao = getByRole('button', { name: /próximo pokémon/i });
 
-  test('Parte 2', () => {
-    const { getByText } = renderpag(<App />);
-    const data = pokemons[0];
+    expect(getByText(pokemons[0].name)).toBeInTheDocument();
 
-    const link = getByText('More');
-    expect(link).toHaveAttribute('href', `/pokemons/${data.id}`);
+    pokemons.forEach((element) => {
+      const nome = getByText(element.name);
+      expect(nome).toBeInTheDocument();
+      userEvent.click(botao);
+    });
   });
-  test('Parte 3', () => {
-    const { getByText, getAllByRole } = renderpag(<App />);
-    const data = pokemons[0];
-
-    const link = getByText('more');
-    fireEvent.click(link);
-    const head = getAllByRole('heading', { level: 2 })[0];
-    expect(head.textContent).toBe(`${data.name} Details`);
+  test('testa quantidade de pokemons', () => {
+    const { container } = renderpag(<App />);
+    const componente = container.getElementsByClassName('pokemon').length;
+    expect(componente).toBe(1);
   });
-  test('Parte 4', () => {
-    const { getByText, history } = renderpag(<App />);
-    const data = pokemons[0];
-
-    const link = getByText('more');
-    fireEvent.click(link);
-    const { pathname } = history.location;
-    expect(pathname).toBe(`/pokemons/${data.id}`);
+  test('busca por tipo', () => {
+    const { getByRole } = renderpag(<App />);
+    const fogo = pokemons.filter((pokemon) => pokemon.type === /fire/i);
+    const botaoDeFogo = getByRole('button', { name: /fire/i });
+    fogo.forEach((pokemon) => {
+      expect(pokemon.name).toBeInTheDocument();
+      userEvent.click(botaoDeFogo);
+    });
   });
-  test('Parte 5', () => {
-    const { getAllByText, getByLabelText, getByAltText } = renderpag(<App />);
-    const { history } = renderpag(<App />);
-    const data = pokemons[0];
+  test('testando botao de filtrar pokemons', () => {
+    const { getByRole, getByText, history } = renderpag(<App />);
+    history.push('/');
+    const filtra = getByRole('button', { name: /all/i });
+    expect(filtra).toBeInTheDocument();
+    expect(filtra).toHaveTextContent('All');
 
-    const link = getAllByText('more')[0];
-    fireEvent.click(link);
+    userEvent.click(filtra);
+    const pikachu = getByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
 
-    const favorite = getByLabelText('Pokémon favoritado?');
-    fireEvent.click(favorite);
-    history.goBack();
-    const img = getByAltText(`${data.name} is marked as favorite`);
-    expect(img).toHaveAttribute('src', '/star-icon.svg');
-    expect(img).toHaveAttribute('alt', `${data.name} is marked as favorite`);
+    const botao = getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(botao);
+    const segundo = pokemons[1];
+    const segundoPokemon = getByText(segundo.name);
+    expect(segundoPokemon).toBeInTheDocument();
   });
 });
-// código inspirado no código do Natanael, turma 8
+
+// Partes do código inspiradas no do Humberto, turma 8
