9c10e7d5ca233f10aae1e9db0930356870805ce2
Pedro Henrique <pedroenrique.400@gmail.com>
2021-03-19 15:56:15 -0300

    Requisito 1 feito

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
@@ -11,4 +13,44 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Verify if the aplication has links', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  const homeLink = getByText(/Home/i);
+  expect(homeLink).toBeInTheDocument();
+
+  userEvent.click(homeLink);
+
+  const { pathname: homepath } = history.location;
+  expect(homepath).toBe('/');
+
+  history.push('/');
+
+  const abouLink = getByText(/About/i);
+  expect(abouLink).toBeInTheDocument();
+
+  userEvent.click(abouLink);
+  const { pathname: aboutpath } = history.location;
+  expect(aboutpath).toBe('/about');
+
+  history.push('/');
+
+  const favoriteLink = getByText(/Favorite Pokémons/i);
+  expect(favoriteLink).toBeInTheDocument();
+
+  userEvent.click(favoriteLink);
+  const { pathname: favoritepath } = history.location;
+  expect(favoritepath).toBe('/favorites');
+});
+
+test('Test if the application is redirected to the Not Found page', () => {
+  const { history, getByText } = renderWithRouter(<App />);
+
+  const route = '/naotemnada';
+  history.push(route);
+
+  const pageNotFound = getByText(/Page requested not found/i);
+  expect(pageNotFound).toBeInTheDocument();
 });
