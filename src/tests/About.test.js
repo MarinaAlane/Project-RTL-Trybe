3bed628417aee19dc83ed9248dc9e3f7686b2fb0
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-03-19 18:40:58 -0300

    Add tests to Pokedex.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,25 +3,25 @@ import { render, screen } from '@testing-library/react';
 import About from '../components/About';
 
 describe('Testing <About />', () => {
-  it('shold contain information about "Pokédex"', () => {
+  it('should contain information about "Pokédex"', () => {
     render(<About />);
     const aboutPokedex = screen.getByText('About Pokédex');
     expect(aboutPokedex).toBeInTheDocument();
   });
 
-  it('shold contain a heading h2 with the text "About Pokédex"', () => {
+  it('should contain a heading h2 with the text "About Pokédex"', () => {
     const { getByRole } = render(<About />);
     const headingH2 = getByRole('heading', { name: /About Pokédex/ });
     expect(headingH2).toBeInTheDocument();
   });
 
-  it('shold contain two paragraphs about "Pokédex"', () => {
+  it('should contain two paragraphs about "Pokédex"', () => {
     const { getAllByText } = render(<About />);
     const pokemonsParagraphs = getAllByText(/Pokémons/);
     expect(pokemonsParagraphs.length).toBe(2);
   });
 
-  it('shold contain a specific image', () => {
+  it('should contain a specific image', () => {
     const { getByAltText } = render(<About />);
     const expectedUrl = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const image = getByAltText(/Pokédex/);
