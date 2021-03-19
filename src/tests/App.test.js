34066456f21c0ce2a6e0c0ba0e261788f421c15c
Christopher <chris.tryber@gmail.com>
2021-03-19 10:17:23 -0300

    inicio do projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -10,7 +10,5 @@ test('renders a reading with the text `Pokédex`', () => {
     </MemoryRouter>,
   );
   const heading = getByText(/Pokédex/i);
-  const path = location.pathname;
   expect(heading).toBeInTheDocument();
-  expect(path).toBe('/');
 });
