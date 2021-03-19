c439649e5b6ced55bd04c33d71d6a164102f8cc3
Rafael Pufal <rafael.pufal23@gmail.com>
2021-03-19 15:57:40 -0300

    requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -13,4 +13,22 @@ describe('tests regarding Pokemon.js component', () => {
     expect(getByRole('img', { name: 'Dragonair sprite' })).toHaveAttribute('src',
       'https://cdn.bulbagarden.net/upload/2/2c/Spr_5b_148.png');
   });
+  it('checks if link directs to correct path', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    const detailsLink = getByRole('link', { name: 'More details' });
+    expect(detailsLink).toHaveAttribute('href', '/pokemons/25');
+    const pikachuPokedexNumber = '25';
+    fireEvent.click(detailsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+    expect(pathname).toContain(pikachuPokedexNumber);
+  });
+  it('checks star icon for favorited pokemon', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    fireEvent.click(getByRole('link', { name: /More details/ }));
+    fireEvent.click(getByRole('checkbox'));
+    const starIcon = getByRole('img', { name: /marked as favorite/ });
+    expect(starIcon).toHaveAttribute('src', '/star-icon.svg');
+    expect(starIcon.alt).toContain('Pikachu');
+  });
 });
