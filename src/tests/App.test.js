8704d71beea9d980b07d1d9561fbe189efe10f49
Khyrki <khyrkimitsunobi@gmail.com>
2021-03-19 18:51:02 -0300

    Cria teste que verifica se há links de navegacao fixos na pagina inicial

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
+/* test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -11,4 +13,40 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+}); */
+describe('Testes aquivo App.js', () => {
+  it('Mostra a Pokédex quando a rota é `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  it('Testa se no topo da aplicação contém um conjunto de links fixos', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const homeLink = getByText('Home');
+    const aboultLink = getByText('About');
+    const favoritePokemonLink = getByText('About');
+    expect(homeLink).toBeInTheDocument();
+    expect(aboultLink).toBeInTheDocument();
+    expect(favoritePokemonLink).toBeInTheDocument();
+  });
+
+  it('Testa se a aplicação é redirecionada p/ pagina inicial, ao clicar em Home', () => {
+    const history = createMemoryHistory();
+    const { getByText } = render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+    const homeLink = getByText('Home');
+    fireEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+  });
 });
