476aa2d04d8dbd4eced175d83403bf5e4e0d92f3
Rafael Pufal <rafael.pufal23@gmail.com>
2021-03-19 15:29:17 -0300

    requisito 5

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -8,5 +8,9 @@ describe('tests regarding Pokemon.js component', () => {
     const { getByRole, getByTestId } = renderWithRouter(<App />);
     fireEvent.click(getByRole('button', { name: 'Dragon' }));
     expect(getByTestId('pokemon-name')).toHaveTextContent('Dragonair');
+    expect(getByTestId('pokemonType')).toHaveTextContent('Dragon');
+    expect(getByTestId('pokemon-weight')).toHaveTextContent('Average weight: 16.5 kg');
+    expect(getByRole('img', { name: 'Dragonair sprite' })).toHaveAttribute('src',
+      'https://cdn.bulbagarden.net/upload/2/2c/Spr_5b_148.png');
   });
 });
