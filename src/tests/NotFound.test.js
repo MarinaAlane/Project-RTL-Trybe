dbebd88d1f41e35453ec8d39f966807f1745020e
marcusWittho <witthoeft.mcs@hotmail.com>
2021-03-19 20:46:42 -0300

    Resolução do requisito 5

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/RenderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testes para o componente <NotFound />', () => {
+  it('Verifica se existe um heding h2 com o texto `Page requested not found 😭`', () => {
+    renderWithRouter(<NotFound />);
+
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      text: /Page requested not found 😭/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Verifica se a página apresenta a imagem do Pikachu', () => {
+    renderWithRouter(<NotFound />);
+
+    const path = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+
+    expect(image).toHaveProperty('src', path);
+  });
+});
