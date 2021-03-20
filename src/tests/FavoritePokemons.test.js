b399323a95bdadb9c21130f98730a8a6c70fd267
ViniciusChagas <viniciushrcs@gmail.com>
2021-03-19 22:30:55 -0300

    Requisito 3, em andamento, issues Lint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+// import { screen } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from '../helper/renderWithRouter';
-import pokemons from '../data';
+// import pokemons from '../data';
 
 describe('Requirement 03, test the FavoritePokemons.js component', () => {
   it('shows the message `No favorite pokemon found`', () => {
