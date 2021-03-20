550b056720de9fdb75855e4046bbc5d3c2a103b9
ViniciusChagas <viniciushrcs@gmail.com>
2021-03-19 22:28:01 -0300

    Requisito 3, em andamento

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../helper/renderWithRouter';
+import pokemons from '../data';
+
+describe('Requirement 03, test the FavoritePokemons.js component', () => {
+  it('shows the message `No favorite pokemon found`', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+});
