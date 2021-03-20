b68ff803523f958e658ffff687795d2cb00ae38c
Carine Becker <caarine.m@gmail.com>
2021-03-19 22:45:56 -0300

    requisito 5 pt1

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import pokemons from '../data'
+import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import isPokemonFavoriteById from '../App';
+
+test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+  const { queryByRole } = renderWithRouter(<Pokedex
+    pokemons={ pokemons }
+    isPokemonFavoriteById={ isPokemonFavoriteById }
+  />);
+
+  const headingPokedex = queryByRole('heading', {
+    level: 2,
+    name: 'Encountered pokémons',
+  });
+  expect(headingPokedex).toBeInTheDocument();
+});
+
