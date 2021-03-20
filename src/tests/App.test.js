c116c5c04dc20793ccb46b5b65370c5ce61ce89a
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-03-20 01:13:59 -0300

    1 requiremente - test names

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+// ----   its off  ----
+
+// test('renders a reading with the text `Pokédex`', () => {
+//   const { getByText } = render(
+//     <MemoryRouter>
+//       <App />
+//     </MemoryRouter>,
+//   );
+//   const heading = getByText(/Pokédex/i);
+//   expect(heading).toBeInTheDocument();
+// });
+
+describe('Teste se a página principal', () => {
+  it('Pokédex é renderizada ao carregar a aplicação no caminho de URL /', () => {
+    // access
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Encountered pokémons');
+
+    // test
+    expect(home).toBeInTheDocument();
+  });
+});
+
+describe('Teste de links no menu', () => {
+  it('O texto dos 3 links devem ser - Home - About - Favorite Pokémons ', () => {
+    // access
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Home');
+    const about = getByText('About');
+    const favPokemons = getByText('Favorite Pokémons');
+
+    // test
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favPokemons).toBeInTheDocument();
+  });
 });
