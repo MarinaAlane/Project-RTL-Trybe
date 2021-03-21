c4bcb7102d42767dd05969ca7d29bbf0cbc65c8f
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-03-21 12:23:12 -0300

    es Lint 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+test('verifica se ao clicar no link about, renderiza a página', () => {
+  const { getByText } = renderWithRouter(<App />);
+  fireEvent.click(getByText(/About/i));
+  // const pathAbout = History.location;
+  // console.log(pathAbout);
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,10 +1,2 @@
-import React from 'react';
-import { render, fireEvent, cleanup } from '@testing-library/react';
-import App from '../App';
-
-afterEach(cleanup);
-
-// arquivo App.test.js pode servir de exemplo
-describe('Teste da aplicação, testando o botão e sua funcionalidade', () => {
-  test('ttt', () => {});
+test('verifica se ao clicar no link about, renderiza a página', () => {
 });
