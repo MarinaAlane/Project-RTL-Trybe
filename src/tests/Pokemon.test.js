3839cff17bb2f59b7e6fab496ba52131908298f6
lcgoandete <lcgoandete@hotmail.com>
2021-03-21 14:01:37 -0300

    fix requisito 6.0 />

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -3,28 +3,32 @@ import { fireEvent } from '@testing-library/dom';
 import renderWithRouter from '../helper/renderWithRouter';
 import App from '../App';
 
-const pokemonNameId = 'pokemon-name';
-const pokemonTypeId = 'pokemonType';
-const pokemonWeightId = 'pokemon-weight';
+const pokemon = {
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+};
 
 describe('Testa o componente <Pokemon.js />', () => {
   it('Testa se é renderizado um card com as informações de determinado pokémon.', () => {
     const { getByTestId, getByAltText } = renderWithRouter(<App />);
-    const pokemonName = getByTestId(pokemonNameId);
-    const pokemonType = getByTestId(pokemonTypeId);
-    const pokemonWeight = getByTestId(pokemonWeightId).textContent;
+    const pokemonName = getByTestId('pokemon-name');
+    const pokemonType = getByTestId('pokemonType');
+    const pokemonWeight = getByTestId('pokemon-weight').textContent;
     const pokemonImg = getByAltText(/Pikachu sprite/i);
 
-    const fifteen = 15;
-    const three = 3;
-    const averageWeight = pokemonWeight.substr(0, fifteen);
-    const valueWeight = pokemonWeight.substr(pokemonWeight.indexOf(':') + 2, three);
-    const measurementUnit = pokemonWeight.substr(pokemonWeight.indexOf('.') + three, 2);
+    const { averageWeight, image, name, type } = pokemon;
+    const { value, measurementUnit } = averageWeight;
 
-    expect(pokemonName).toHaveTextContent('Pikachu');
-    expect(pokemonType).toHaveTextContent('Electric');
-    expect(pokemonWeight).toBe(`${averageWeight} ${valueWeight} ${measurementUnit}`);
-    expect(pokemonImg.src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(pokemonName).toHaveTextContent(name);
+    expect(pokemonType).toHaveTextContent(type);
+    expect(pokemonWeight).toBe(`Average weight: ${value} ${measurementUnit}`);
+    expect(pokemonImg.src).toBe(image);
   });
 
   it(`Testa se o card do Pokémon indicado na Pokédex contém um link de navegação
