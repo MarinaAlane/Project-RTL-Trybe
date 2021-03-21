d79793d5cdd1aabf44f5960ceda8e0783aa01580
edison-ps <edison@osite.com.br>
2021-03-21 14:13:15 -0300

    Requisito 7 V1.01

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -5,11 +5,13 @@ import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import pokemons from '../data';
 
+const moreDetails = 'More details';
+
 describe('PokemonDetails.js', () => {
-  test('Verify  display of pokémon informations details ', () => {
+  test('Verify display of pokémon informations details ', () => {
     renderWithRouter(<App />);
 
-    const linkDetails = screen.getByText('More details');
+    const linkDetails = screen.getByText(moreDetails);
     expect(linkDetails).toBeInTheDocument();
 
     userEvent.click(linkDetails);
@@ -33,5 +35,55 @@ describe('PokemonDetails.js', () => {
     expect(summary).toBeInTheDocument();
   });
 
-  
+  test('Verify maps section ', () => {
+    renderWithRouter(<App />);
+
+    const linkDetails = screen.getByText(moreDetails);
+    expect(linkDetails).toBeInTheDocument();
+
+    userEvent.click(linkDetails);
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toBeInTheDocument();
+
+    const messageDetails = screen.getAllByRole('heading', { level: 2 });
+    const mapText = `Game Locations of ${pokemonName.innerHTML}`;
+    const mapTitle = messageDetails.find((text) => text.innerHTML === mapText);
+    expect(mapTitle.innerHTML).toBe(mapText);
+
+    const selectPoke = pokemons.find((pokemon) => pokemon.name === pokemonName.innerHTML);
+    const foundAt = selectPoke.foundAt.length;
+    const images = screen.getAllByRole('img');
+    const altText = `${pokemonName.innerHTML} location`;
+    const imagesMap = images.filter((image) => image.alt === altText);
+    expect(imagesMap.length).toBe(foundAt);
+
+    selectPoke.foundAt.forEach((local) => {
+      const localTitle = screen.getByText(local.location);
+      expect(localTitle).toBeInTheDocument();
+      const imageSrc = images.find((image) => image.src === local.map);
+      expect(imageSrc).toBeInTheDocument();
+    });
+  });
+
+  test('Verify Pokemon favorite ', () => {
+    renderWithRouter(<App />);
+
+    const linkDetails = screen.getByText(moreDetails);
+    expect(linkDetails).toBeInTheDocument();
+
+    userEvent.click(linkDetails);
+
+    const checkBox = screen.getByRole('checkbox');
+    expect(checkBox).toBeInTheDocument();
+
+    userEvent.click(checkBox);
+    expect(checkBox.checked).toBe(true);
+
+    userEvent.click(checkBox);
+    expect(checkBox.checked).toBe(false);
+
+    const favoriteLabel = screen.getByText('Pokémon favoritado?');
+    expect(favoriteLabel).toBeInTheDocument();
+  });
 });
