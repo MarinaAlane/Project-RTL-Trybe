f65b384174a591ce4bb80baf18e08b6f00f732dc
edison-ps <edison@osite.com.br>
2021-03-20 21:37:44 -0300

    Requisito 5 V1.01

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { getByText, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import pokemons from '../data';
-import { pokemonType } from '../types';
+
+const testIdName = 'pokemon-name';
+const testIdType = 'pokemonType';
 
 describe('Pokedex.js', () => {
   test('Verify message o Encountered pokémons', () => {
@@ -17,30 +19,30 @@ describe('Pokedex.js', () => {
   test('Verify display next pokémon', () => {
     renderWithRouter(<App />);
 
-    const buttons = screen.getAllByRole('button');
-    expect(buttons[buttons.length - 1].innerHTML).toBe('Próximo pokémon');
+    const button = screen.getByTestId('next-pokemon');
+    expect(button.innerHTML).toBe('Próximo pokémon');
 
     for (let index = 0; index < pokemons.length; index += 1) {
-      userEvent.click(buttons[buttons.length - 1]);
+      userEvent.click(button);
     }
 
-    const firstPokemon = screen.getByTestId('pokemon-name');
+    const firstPokemon = screen.getByTestId(testIdName);
     expect(firstPokemon.innerHTML).toBe(pokemons[0].name);
 
     const clickTimes = Math.round(Math.random() * pokemons.length - 1);
 
     for (let index = 0; index < clickTimes; index += 1) {
-      userEvent.click(buttons[buttons.length - 1]);
+      userEvent.click(button);
     }
 
-    const lastPokemon = screen.getByTestId('pokemon-name');
+    const lastPokemon = screen.getByTestId(testIdName);
     expect(lastPokemon.innerHTML).toBe(pokemons[clickTimes].name);
   });
 
   test('Verify one pokémon for time', () => {
     renderWithRouter(<App />);
 
-    const pokemon = screen.getAllByTestId('pokemon-name');
+    const pokemon = screen.getAllByTestId(testIdName);
     expect(pokemon.length).toBe(1);
   });
 
@@ -49,20 +51,37 @@ describe('Pokedex.js', () => {
 
     const pokeType = Math.round(Math.random() * 2);
     const arrayButtons = screen.getAllByTestId('pokemon-type-button');
-console.log(pokeType);
+
     if (pokeType === 0) {
       const buttonFilter = arrayButtons.find((button) => button.innerHTML === 'Fire');
       userEvent.click(buttonFilter);
       const pokemon = screen.getByTestId('pokemonType');
-console.log(pokemon.innerHTML);
       expect(pokemon.innerHTML).toBe('Fire');
     } else {
       const buttonFilter = arrayButtons.find((button) => button.innerHTML === 'Psychic');
       userEvent.click(buttonFilter);
       const pokemon = screen.getByTestId('pokemonType');
-console.log(pokemon.innerHTML);
       expect(pokemon.innerHTML).toBe('Psychic');
     }
   });
 
+  test('Verify reset filter button', () => {
+    renderWithRouter(<App />);
+
+    const resetButton = screen.getByTestId('');
+    expect(resetButton.innerHTML).toBe('All');
+
+    userEvent.click(resetButton);
+    const firstType = screen.getByTestId(testIdType);
+    expect(firstType.innerHTML).toBe('Electric');
+
+    const nextPokemon = screen.getByTestId('next-pokemon');
+    userEvent.click(nextPokemon);
+    const secondType = screen.getByTestId(testIdType);
+    expect(secondType.innerHTML).toBe('Fire');
+
+    userEvent.click(nextPokemon);
+    const thirdType = screen.getByTestId(testIdType);
+    expect(thirdType.innerHTML).toBe('Bug');
+  });
 });
