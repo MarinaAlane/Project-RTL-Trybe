7947f1ffcab9783a7d6a1e8d2d5088fab1b26319
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-03-21 11:49:58 -0300

    task1 read app title Pokemon

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+  const { getByText } = renderWithRouter(<App />);
+  const titleApp = getByText(/Pokédex/i);
+  expect(titleApp).toBeInTheDocument();
 });
