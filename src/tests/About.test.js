fb6a36116f2ea57276867ce3e1e143b1ea6e02f5
Khyrki <khyrkimitsunobi@gmail.com>
2021-03-21 15:00:32 -0300

    Cria testes que verificam informacoes da página about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import Aboult from '../components/About';
+
+describe('Bloco de testes para o componente about', () => {
+  it('Testa se a página contem as informacoes da Pokédex', () => {
+    const { getByText, getByAltText } = render(<Aboult />);
+    const aboutTitle = getByText('About Pokédex');
+    const aboutParagraph = getByText(/This application simulates a Pokédex/i);
+    const aboutImage = getByAltText('Pokédex');
+
+    expect(aboutTitle.textContent).toBe('About Pokédex');
+    expect(aboutParagraph.textContent).toMatch(/This application simulates a Pokédex/i);
+    expect(aboutImage.alt).toBe('Pokédex');
+  });
+});
