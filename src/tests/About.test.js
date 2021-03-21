21d2a11398f460deb81fea0ea199e13d04c23676
Khyrki <khyrkimitsunobi@gmail.com>
2021-03-21 15:26:30 -0300

    Cria testes que afere se existe um titulo na pagina, dois paragrafos, e uma imagem

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -7,10 +7,33 @@ describe('Bloco de testes para o componente about', () => {
     const { getByText, getByAltText } = render(<Aboult />);
     const aboutTitle = getByText('About Pokédex');
     const aboutParagraph = getByText(/This application simulates a Pokédex/i);
-    const aboutImage = getByAltText('Pokédex');
 
     expect(aboutTitle.textContent).toBe('About Pokédex');
     expect(aboutParagraph.textContent).toMatch(/This application simulates a Pokédex/i);
-    expect(aboutImage.alt).toBe('Pokédex');
+  });
+
+  it('Testa de se a página contem um h2 com o texto "About Pokédex"', () => {
+    const { getByRole } = render(<Aboult />);
+    const aboutH2 = getByRole('heading', { level: 2 });
+
+    expect(aboutH2).toBeInTheDocument();
+  });
+
+  it('Testa se a página contem dois parágrafos com textos sobre a Pókedex', () => {
+    const { getByText } = render(<Aboult />);
+    const firstAboutParagraph = getByText(/This application simulates/i);
+    const secondAboutParagraph = getByText(/One can filter Pokémons/i);
+
+    expect(firstAboutParagraph.textContent).toMatch(/This application simulates/i);
+    expect(secondAboutParagraph.textContent).toMatch(/One can filter Pokémons/i);
+  });
+
+  it('Testa se a pagina contem uma imagem', () => {
+    const { getByRole } = render(<Aboult />);
+    const aboutImage = getByRole('img');
+
+    expect(aboutImage.src).toBe(
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+    );
   });
 });
