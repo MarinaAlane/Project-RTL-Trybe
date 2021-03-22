7925ba214adc2df08cb7bcdefd5c5491c3dfd0b0
Thiago Granville <thiagogranville@gmail.com>
2021-03-22 03:52:17 -0300

    Requisito 3: Testa componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,56 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('Test FavoritePokemons component', () => {
+  test('Renders `No favorite pokemon found` if the favorite list is empty', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+    const notFoundMessage = getByText('No favorite pokemon found');
+
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+
+  test('All favorited pokemons cards are rendered', () => {
+    const { getByText, getByLabelText } = renderWithRouter(<App />);
+
+    const moreDetailsLink = getByText(/More details/i);
+
+    userEvent.click(moreDetailsLink);
+
+    const favoritePokemonCheckbox = getByLabelText(/Pokémon favoritado/i);
+
+    userEvent.click(favoritePokemonCheckbox);
+
+    const favoritePokemonLink = getByText(/Favorite Pokémons/i);
+    
+    userEvent.click(favoritePokemonLink);
+
+    const favoritedPokemon = getByText(/pikachu/i);
+
+    expect(favoritedPokemon).toBeInTheDocument();
+  });
+
+  test('Dont render a pokemon card if it is not favorited', () => {
+    const { getByText, getByLabelText, queryByText } = renderWithRouter(<App />);
+
+    const moreDetailsLink = getByText(/More details/i);
+
+    userEvent.click(moreDetailsLink);
+
+    const favoritePokemonCheckbox = getByLabelText(/Pokémon favoritado/i);
+
+    userEvent.click(favoritePokemonCheckbox);
+
+    const favoritePokemonLink = getByText(/Favorite Pokémons/i);
+    
+    userEvent.click(favoritePokemonLink);
+
+    const favoritedPokemon = queryByText(/pikachu/i);
+
+    expect(favoritedPokemon).not.toBeInTheDocument();
+  });
+
+});
