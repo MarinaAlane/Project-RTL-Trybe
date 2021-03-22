7ea0df7ec2028c6ed20c89d0773a6f9d53aacf30
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-03-22 14:53:02 -0300

    Test the `<Pokemon.js />` component

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Test the `<Pokemon.js />` component', () => {
+  it('a card with the information of a certain Pokémon is rendered', () => {
+    const { getByTestId, getByRole } = renderWithRouter(<App />);
+    expect(getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
+    expect(getByTestId('pokemonType')).toHaveTextContent('Electric');
+    expect(getByTestId('pokemon-weight')).toHaveTextContent('6.0 kg');
+    const image = getByRole('img');
+    expect(image.src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(image.alt).toBe('Pikachu sprite');
+  });
+
+  it('the Pokémon card indicated contains a navigation link to view details', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText('More details')).toHaveAttribute('href', '/pokemons/25');
+  });
+
+  it('there is a star icon on favorite Pokémon', () => {
+    const { getByText, getByAltText } = renderWithRouter(<App />);
+    userEvent.click(getByText('More details'));
+    userEvent.click(getByText(/Pokémon favoritado/i));
+    const favoritedIcon = getByAltText('Pikachu is marked as favorite');
+    expect(favoritedIcon).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
