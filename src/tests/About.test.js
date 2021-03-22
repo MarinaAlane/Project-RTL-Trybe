4b098cc45b7349d57a7fc0a729ff103f3a3777ed
tiago-yoneda <xyonedax@gmail.com>
2021-03-22 12:12:43 -0300

    teste 2.1 completed

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+import About from '../components/About';
+
+describe('Tests for the About component',() => {
+  it('should show a page containing the Pokédex info', () => {
+    const { getByText } = renderWithRouter(<About />);
+
+    expect(getByText(/About Pokédex/i)).toBeInTheDocument();
+  })
+});
