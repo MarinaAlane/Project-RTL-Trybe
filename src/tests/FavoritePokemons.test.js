9516623c4139c6413e7b394af53fbe66d84186b7
Micaela Alves <micaela.alves93@gmail.com>
2021-03-22 05:03:35 -0300

    Req 3 - cards de pokémons favoritados

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
 describe('Test the FavoritePokemons.js component', () => {
   test('No favorite pokemon found message is displayed', () => {
@@ -8,7 +10,25 @@ describe('Test the FavoritePokemons.js component', () => {
     expect(getByText('No favorite pokemon found')).toBeInTheDocument();
   });
 
-/*   test('todos', () => {
-    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
-  }); */
+  test('all favorite Pokémon cards are displayed', () => {
+    const { getAllByRole, getByRole } = renderWithRouter(<App />);
+    const linkMoreDetails = getByRole('link', { name: /More details/i });
+    expect(linkMoreDetails).toBeInTheDocument();
+    userEvent.click(linkMoreDetails);
+    const checkFav = getByRole('checkbox');
+    expect(checkFav).toBeInTheDocument();
+    userEvent.click(checkFav);
+    const homeLink = getByRole('link', { name: /Home/i });
+    expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+    const nextPokemon = getByRole('button', { name: /Próximo pokémon/i });
+    expect(nextPokemon).toBeInTheDocument();
+    userEvent.click(nextPokemon);
+    userEvent.click(linkMoreDetails);
+    userEvent.click(checkFav);
+    const favLink = getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(favLink);
+    const qntFav = getAllByRole('img');
+    expect(qntFav.length).toEqual(2);
+  });
 });
