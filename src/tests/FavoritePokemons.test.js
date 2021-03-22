111b6638a031101f3ff0948f13515ecc4ef1f733
Micaela Alves <micaela.alves93@gmail.com>
2021-03-22 00:58:55 -0300

    Req 3 - Teste se é exibido na tela a mensagem 

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Test the FavoritePokemons.js component', () => {
+  test('No favorite pokemon found message is displayed', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+
+/*   test('todos', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+  }); */
+});
