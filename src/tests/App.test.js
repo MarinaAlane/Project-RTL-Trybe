0585695e8dc946becec34817693ee37802119838
RodrigoNeoEx <draja.acc@gmail.com>
2021-03-22 11:54:48 -0300

    URL match tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import renderRoute from '../services/renderRoute';
+import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
 describe('testes do  componente App.js', () => {
@@ -28,18 +28,8 @@ describe('testes do  componente App.js', () => {
     },
   );
 
-  it(
-    'Página principal da Pokédex deve renderizar no caminho de URL / onCLick Home',
-    () => {
-      const { getByText, history } = renderRoute(<App />);
-      const homeRouteTest = getByText(/Home/i);
-      userEvent.click(homeRouteTest);
-      expect(history.location.pathname).toBe('/');
-    },
-  );
-
   it('Topo da aplicação deve conter links Home, About e Favorite Pokémons', () => {
-    const { getByRole } = renderRoute(<App />);
+    const { getByRole } = renderWithRouter(<App />);
     const homeLink = getByRole('link', { name: 'Home' });
     const aboutLink = getByRole('link', { name: 'About' });
     const favoriteLink = getByRole('link', { name: 'Favorite Pokémons' });
@@ -48,4 +38,44 @@ describe('testes do  componente App.js', () => {
     expect(aboutLink).toBeInTheDocument();
     expect(favoriteLink).toBeInTheDocument();
   });
+
+  it(
+    'Deve renderizar a URL / com evento onCLick no link Home',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const homeRouteTest = getByText(/Home/i);
+      userEvent.click(homeRouteTest);
+      expect(history.location.pathname).toBe('/');
+    },
+  );
+
+  it(
+    'Deve renderizar a URL /about com evento onCLick no link About',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const aboutRouteTest = getByText(/About/i);
+      userEvent.click(aboutRouteTest);
+      expect(history.location.pathname).toBe('/about');
+    },
+  );
+
+  it(
+    'Deve renderizar a URL /favorites onCLick Favorite Pokémons',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      const favoriteRouteTest = getByText(/Favorite/i);
+      userEvent.click(favoriteRouteTest);
+      expect(history.location.pathname).toBe('/favorites');
+    },
+  );
+
+  it(
+    'Deve renderizar a URL /notFound se for uma URL desconhecida',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
+      history.push('/notFound');
+      const notFound = getByText(/Not Found/i);
+      expect(notFound).toBeInTheDocument();
+    },
+  );
 });
