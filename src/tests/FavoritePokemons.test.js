eca34d474ea06a5e4705bbfc575222bc67125637
Uibirá <usbarreto@gmail.com>
2021-03-22 17:18:27 -0300

    Req.2

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,6 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter'; 
+
+test('Renders `No favorite pokemon found` if there are not favorite pokémons', () => {});
