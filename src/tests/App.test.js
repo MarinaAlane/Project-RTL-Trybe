45f284d26bb38363cb5af7e7b7f0c3afc6821951
Enio Nicael <enionicael@gmail.com>
2021-03-22 16:15:53 -0300

    Adicao do arquivo renderWithRouter.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,22 +3,26 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('Renderiza um cabecalho com o texto `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Comportamentos do App.', () => {
+  test('Renderiza um cabecalho com o texto `Pokédex`', () => {
 
-test('Mostra a Pokédex quando o route for `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+    const { getByText } = render(<App />);
+    /* const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    ); */
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  test('Mostra a Pokédex quando o route for `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
 });
