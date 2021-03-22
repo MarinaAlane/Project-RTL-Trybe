8a78bf78ab069b41a2dd7263540c73842008042a
Pedro Toselli <pedrotrybe@gmail.com>
2021-03-22 18:13:11 -0300

    Requisito 2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,82 +1,3 @@
-// import React from 'react';
-// import { fireEvent } from '@testing-library/dom';
-// import renderWithRouter from '../helpers/renderWithRouter';
-// import App from '../App';
-
-// describe('Testando o componente App', () => {
-//   it('Teste se Pokédex é renderizada ao carregar o caminho de URL /', () => {
-//     const { getByText, history } = renderWithRouter(<App />);
-//     const pokedexHeader = getByText(/Encountered pokémons/i);
-//     const { location } = history;
-
-//     expect(pokedexHeader).toBeInTheDocument();
-//     expect(location.pathname).toBe('/');
-//   });
-
-//   it('Teste se a aplicação contém um conjunto de links de navegação.', () => {
-//     const { getAllByRole } = renderWithRouter(<App />);
-//     const linksAvailable = getAllByRole('link');
-//     const linksLenght = 4;
-
-//     expect(linksAvailable.length).toBe(linksLenght);
-
-//     expect(linksAvailable[0]).toBeInTheDocument();
-//     expect(linksAvailable[0].innerHTML).toBe('Home');
-
-//     expect(linksAvailable[1]).toBeInTheDocument();
-//     expect(linksAvailable[1].innerHTML).toBe('About');
-
-//     expect(linksAvailable[2]).toBeInTheDocument();
-//     expect(linksAvailable[2].innerHTML).toBe('Favorite Pokémons');
-
-//     expect(linksAvailable[3]).toBeInTheDocument();
-//     expect(linksAvailable[3].innerHTML).toBe('More details');
-//   });
-
-//   it('Teste se o link Home redireciona para /', () => {
-//     const { getByText, history } = renderWithRouter(<App />);
-//     const homeLink = getByText('Home');
-
-//     fireEvent.click(homeLink);
-
-//     const { location } = history;
-
-//     expect(location.pathname).toBe('/');
-//   });
-
-//   it('Teste se o link About redireciona para /about', () => {
-//     const { getByText, history } = renderWithRouter(<App />);
-//     const aboutLink = getByText('About');
-
-//     fireEvent.click(aboutLink);
-
-//     const { location } = history;
-
-//     expect(location.pathname).toBe('/about');
-//   });
-
-//   it('Teste se o link Favorite Pokémons redireciona para /favorites', () => {
-//     const { getByText, history } = renderWithRouter(<App />);
-//     const favoriteLinks = getByText('Favorite Pokémons');
-
-//     fireEvent.click(favoriteLinks);
-
-//     const { location } = history;
-
-//     expect(location.pathname).toBe('/favorites');
-//   });
-
-//   it('Teste se uma URL desconhecida redireciona para a página NotFound', () => {
-//     const { getByText, history } = renderWithRouter(<App />);
-
-//     history.push('urlivalida');
-
-//     const notFoundMessage = getByText(/not found/i);
-
-//     expect(notFoundMessage).toBeInTheDocument();
-//   });
-// });
-
 import React from 'react';
 import { fireEvent } from '@testing-library/dom';
 import renderWithRouter from '../helpers/renderWithRouter';
@@ -94,29 +15,29 @@ describe('Testando o componente App', () => {
 
   it('Teste se a aplicação contém um conjunto de links de navegação.', () => {
     const { getAllByRole } = renderWithRouter(<App />);
-    const arrayOfLinks = getAllByRole('link');
-    const numberOfExpectedLinks = 4;
+    const linksAvailable = getAllByRole('link');
+    const linksLenght = 4;
 
-    expect(arrayOfLinks.length).toBe(numberOfExpectedLinks);
+    expect(linksAvailable.length).toBe(linksLenght);
 
-    expect(arrayOfLinks[0]).toBeInTheDocument();
-    expect(arrayOfLinks[0].innerHTML).toBe('Home');
+    expect(linksAvailable[0]).toBeInTheDocument();
+    expect(linksAvailable[0].innerHTML).toBe('Home');
 
-    expect(arrayOfLinks[1]).toBeInTheDocument();
-    expect(arrayOfLinks[1].innerHTML).toBe('About');
+    expect(linksAvailable[1]).toBeInTheDocument();
+    expect(linksAvailable[1].innerHTML).toBe('About');
 
-    expect(arrayOfLinks[2]).toBeInTheDocument();
-    expect(arrayOfLinks[2].innerHTML).toBe('Favorite Pokémons');
+    expect(linksAvailable[2]).toBeInTheDocument();
+    expect(linksAvailable[2].innerHTML).toBe('Favorite Pokémons');
 
-    expect(arrayOfLinks[3]).toBeInTheDocument();
-    expect(arrayOfLinks[3].innerHTML).toBe('More details');
+    expect(linksAvailable[3]).toBeInTheDocument();
+    expect(linksAvailable[3].innerHTML).toBe('More details');
   });
 
   it('Teste se o link Home redireciona para /', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const linkToHome = getByText('Home');
+    const homeLink = getByText('Home');
 
-    fireEvent.click(linkToHome);
+    fireEvent.click(homeLink);
 
     const { location } = history;
 
@@ -125,9 +46,9 @@ describe('Testando o componente App', () => {
 
   it('Teste se o link About redireciona para /about', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const linkToAbout = getByText('About');
+    const aboutLink = getByText('About');
 
-    fireEvent.click(linkToAbout);
+    fireEvent.click(aboutLink);
 
     const { location } = history;
 
@@ -136,9 +57,9 @@ describe('Testando o componente App', () => {
 
   it('Teste se o link Favorite Pokémons redireciona para /favorites', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const linkToFavorites = getByText('Favorite Pokémons');
+    const favoriteLinks = getByText('Favorite Pokémons');
 
-    fireEvent.click(linkToFavorites);
+    fireEvent.click(favoriteLinks);
 
     const { location } = history;
 
@@ -150,8 +71,8 @@ describe('Testando o componente App', () => {
 
     history.push('urlivalida');
 
-    const notFoundText = getByText(/not found/i);
+    const notFoundMessage = getByText(/not found/i);
 
-    expect(notFoundText).toBeInTheDocument();
+    expect(notFoundMessage).toBeInTheDocument();
   });
 });
