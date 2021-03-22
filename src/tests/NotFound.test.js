bea1db6da0aca493f48a2135b1d1e816c69406a4
tiago-yoneda <xyonedax@gmail.com>
2021-03-22 13:12:08 -0300

    teste 4.1 completed

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Tests for the NotFound component', () => {
+  it('should show a h2 with the text "Page requested not Found"', () => {
+    const { getByRole, getByText } = renderWithRouter(<NotFound />);
+
+    const heading2 = getByRole('heading', {
+      level: 2,
+    });
+
+    expect(heading2).toBeInTheDocument();
+    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+})
