3333f1661e35e737badbb3363730ed8854c8ff72
Micaela Alves <micaela.alves93@gmail.com>
2021-03-22 01:10:54 -0300

    Req 4 - Contém o heading h2  Encountered pokémons

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../components/Pokedex';
+
+describe('Test the Pokedex.js component', () => {
+  test('the page contains an h2 heading', () => {
+    const { getByRole } = renderWithRouter(<Pokedex />);
+    expect(getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    })).toBeInTheDocument();
+  });
+});
