6a68bb3626c9224fcfa6ec9d6c10ba61b6d7349c
tiago-yoneda <xyonedax@gmail.com>
2021-03-22 13:57:11 -0300

    teste 5.2 completed

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,7 +4,7 @@ import NotFound from '../components/NotFound';
 
 describe('Tests for the NotFound component', () => {
   it('should show a h2 with the text "Page requested not Found"', () => {
-    const { getByRole, getByText } = renderWithRouter(<NotFound />);
+    const { getByRole } = renderWithRouter(<NotFound />);
 
     const heading2 = getByRole('heading', {
       level: 2,
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,15 +2,28 @@ import React from 'react';
 import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../helper/renderWithRouter';
 import App from '../App';
-import pokemons from '../data'
+import pokemons from '../data';
 
 describe('Tests for the Pokedex component', () => {
   it('should have a h2 with the text "Encountered pokémons"', () => {
-    const { getByRole, getByText } = renderWithRouter(<App />);
+    const { getByRole } = renderWithRouter(<App />);
     const heading2 = getByRole('heading', {
       level: 2,
       name: /Encountered pokémons/i,
-    })
+    });
     expect(heading2).toBeInTheDocument();
+  });
+
+  it('should show the next pokemon when "Proximo pokémon" is clicked,'
+  + ' and go back to the first after the last one is clicked', () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+
+    const nextPokemonButton = getByTestId('next-pokemon');
+    expect(nextPokemonButton).toBeInTheDocument();
+    pokemons.forEach((pokemon) => {
+      expect(getByText(pokemon.name)).toBeInTheDocument();
+      fireEvent.click(nextPokemonButton);
+    });
+    expect(getByText(/Pikachu/i)).toBeInTheDocument();
   });
 });
