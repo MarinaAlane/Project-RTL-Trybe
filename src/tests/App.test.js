afe49b134b06b8669e2eec69af08847d5a124b54
Pedro Toselli <pedrotrybe@gmail.com>
2021-03-22 16:32:03 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,48 +3,37 @@ import { fireEvent } from '@testing-library/dom';
 import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
-// test('renders a reading with the text `Pokédex`', () => {
-//   const { getByText } = render(
-//     <MemoryRouter>
-//       <App />
-//     </MemoryRouter>,
-//   );
-//   const heading = getByText(/Pokédex/i);
-//   expect(heading).toBeInTheDocument();
-// });
-
-describe('', () => {
-
-  it('Teste se a página principal da Pokédex é renderizada ao carregar a aplicação no caminho de URL /', () => {
+describe('Testando o componente App', () => {
+  it('Teste se Pokédex é renderizada ao carregar o caminho de URL /', () => {
     const { getByText, history } = renderWithRouter(<App />);
-    const pokedexHead = getByText(/Encountered pokémons/i);
+    const pokedexHeader = getByText(/Encountered pokémons/i);
     const { location } = history;
 
-    expect(pokedexHead).toBeInTheDocument();
+    expect(pokedexHeader).toBeInTheDocument();
     expect(location.pathname).toBe('/');
-  })
-
-  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação, home, about e favorites', () => {
-    const { getAllByRole } = renderWithRouter(<App />);
-    const linksAvlb = getAllByRole('link');
-    const linksLength = 4;
-
-    expect(linksAvlb.length).toBe(linksLength);
-
-    expect(linksAvlb[0]).toBeInTheDocument();
-    expect(linksAvlb[0].innerHTML).toBe('Home');
-
-    expect(linksAvlb[1]).toBeInTheDocument();
-    expect(linksAvlb[1].innerHTML).toBe('About');
-
-    expect(linksAvlb[2]).toBeInTheDocument();
-    expect(linksAvlb[2].innerHTML).toBe('Favorite Pokémons');
-
-    expect(linksAvlb[3]).toBeInTheDocument();
-    expect(linksAvlb[3].innerHTML).toBe('More details');
   });
 
-  it('Teste se a aplicação é redirecionada para a página inicial, na URL / ao clicar no link Home da barra de navegação', () => {
+  it('Teste se a aplicação contém um conjunto de links de navegação.', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const linksAvailable = getAllByRole('link');
+    const linksLenght = 4;
+
+    expect(linksAvailable.length).toBe(linksLenght);
+
+    expect(linksAvailable[0]).toBeInTheDocument();
+    expect(linksAvailable[0].innerHTML).toBe('Home');
+
+    expect(linksAvailable[1]).toBeInTheDocument();
+    expect(linksAvailable[1].innerHTML).toBe('About');
+
+    expect(linksAvailable[2]).toBeInTheDocument();
+    expect(linksAvailable[2].innerHTML).toBe('Favorite Pokémons');
+
+    expect(linksAvailable[3]).toBeInTheDocument();
+    expect(linksAvailable[3].innerHTML).toBe('More details');
+  });
+
+  it('Teste se o link Home redireciona para /', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const homeLink = getByText('Home');
 
@@ -55,7 +44,7 @@ describe('', () => {
     expect(location.pathname).toBe('/');
   });
 
-  it('Teste se a aplicação é redirecionada para a página de About, na URL /about, ao clicar no link About da barra de navegação', () => {
+  it('Teste se o link About redireciona para /about', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const aboutLink = getByText('About');
 
@@ -66,7 +55,7 @@ describe('', () => {
     expect(location.pathname).toBe('/about');
   });
 
-  it('Teste se a aplicação é redirecionada para a página de Pokémons Favoritados, na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação', () => {
+  it('Teste se o link Favorite Pokémons redireciona para /favorites', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const favoriteLinks = getByText('Favorite Pokémons');
 
@@ -77,15 +66,13 @@ describe('', () => {
     expect(location.pathname).toBe('/favorites');
   });
 
-  it('Teste se a aplicação é redirecionada para a página Not Found ao entrar em uma URL desconhecida', () => {
+  it('Teste se uma URL desconhecida redireciona para a página NotFound', () => {
     const { getByText, history } = renderWithRouter(<App />);
 
     history.push('urlivalida');
 
-    const notfoundMessage = getByText(/not found/i);
+    const notFoundMessage = getByText(/not found/i);
 
-    expect(notfoundMessage).toBeInTheDocument();
-  })
-
-  it('')
-})
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+});
