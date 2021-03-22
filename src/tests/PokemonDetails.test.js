806823a7167f746992e3e908250fd4892f7b7ad3
tiago-yoneda <xyonedax@gmail.com>
2021-03-22 16:08:07 -0300

    teste 7.2 completed

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -18,11 +18,41 @@ describe('Testes for the PokemonDetails component', () => {
     expect(detailsButton).not.toBeInTheDocument();
 
     const summary = getByRole('heading', {
+      level: 2,
       name: /Summary/i,
     });
 
     expect(summary).toBeInTheDocument();
 
     expect(getByText(/roasts hard berries/i)).toBeInTheDocument();
+  });
+
+  it('should show map info about the pokemon', () => {
+    const { getByRole, getAllByAltText } = renderWithRouter(<App />);
+
+    const detailsButton = getByRole('link', {
+      name: /more details/i,
+    });
+    expect(detailsButton).toBeInTheDocument();
+
+    fireEvent.click(detailsButton);
+
+    const locationsHeading = getByRole('heading', {
+      level: 2,
+      name: /Game Locations/i,
+    });
+
+    expect(locationsHeading).toBeInTheDocument();
+
+    const locationMap = getAllByAltText(/Pikachu location/i);
+    const url = [
+      'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+    ];
+    expect(locationMap).toHaveLength(2);
+    locationMap.forEach((map, index) => {
+      expect(map).toBeInTheDocument();
+      expect(map.src).toBe(url[index]);
+    });
   });
 });
