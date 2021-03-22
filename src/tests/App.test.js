8e6a0b6858e773b6a4fda9ea98d389ed555df1e8
RodrigoNeoEx <draja.acc@gmail.com>
2021-03-22 10:48:23 -0300

    test onClick btnHome render on route '/'

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import renderRoute from '../services/renderRoute';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
+describe('testes do  componente App.js', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it(
+    'Página principal da Pokédex deve renderizar no caminho de URL / onCLick Home',
+    () => {
+      const { getByText, history } = renderRoute(<App />);
+      const homeRouteTest = getByText(/Home/i);
+      userEvent.click(homeRouteTest);
+      expect(history.location.pathname).toBe('/');
+    },
   );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
 });
