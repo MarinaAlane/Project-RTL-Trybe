70d8db6c8187e94d7c08e97bd4092d93eca33b1a
RodrigoNeoEx <draja.acc@gmail.com>
2021-03-22 16:14:56 -0300

    test if have h2 with right text  in component about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -10,4 +10,11 @@ describe('testes do componente FavoritePokemons.js', () => {
     expect(info).toBeInTheDocument();
     expect(info2).toBeInTheDocument();
   });
+
+  test('A página deve conter um heading h2 com o texto About Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const h2 = getByRole('heading', {
+      level: 2, name: 'About Pokédex' });
+    expect(h2).toBeInTheDocument();
+  });
 });
