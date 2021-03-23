b313f782a6c28d21bffabdcad59d609cfd5772a5
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-03-23 10:59:33 -0300

    Test the `<Pokemon.js />` component - refactoring

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -2,21 +2,33 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+import pokemons from '../data';
 
 describe('Test the `<Pokemon.js />` component', () => {
+  const nextPokemon = 'Próximo pokémon';
+
   it('a card with the information of a certain Pokémon is rendered', () => {
-    const { getByTestId, getByRole } = renderWithRouter(<App />);
-    expect(getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
-    expect(getByTestId('pokemonType')).toHaveTextContent('Electric');
-    expect(getByTestId('pokemon-weight')).toHaveTextContent('6.0 kg');
-    const image = getByRole('img');
-    expect(image.src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
-    expect(image.alt).toBe('Pikachu sprite');
+    const { getByTestId, getByText, getByRole } = renderWithRouter(<App />);
+    pokemons.forEach((pokemon) => {
+      const { name, type, image } = pokemon;
+      const { value, measurementUnit } = pokemon.averageWeight;
+      expect(getByTestId('pokemon-name')).toHaveTextContent(name);
+      expect(getByTestId('pokemonType')).toHaveTextContent(type);
+      expect(getByTestId('pokemon-weight')).toHaveTextContent(value, measurementUnit);
+      const pokemonImage = getByRole('img');
+      expect(pokemonImage.src).toBe(image);
+      expect(pokemonImage.alt).toBe(`${name} sprite`);
+      userEvent.click(getByText(nextPokemon));
+    });
   });
 
   it('the Pokémon card indicated contains a navigation link to view details', () => {
     const { getByText } = renderWithRouter(<App />);
-    expect(getByText('More details')).toHaveAttribute('href', '/pokemons/25');
+    pokemons.forEach((pokemon) => {
+      const { id } = pokemon;
+      expect(getByText('More details')).toHaveAttribute('href', `/pokemons/${id}`);
+      userEvent.click(getByText(nextPokemon));
+    });
   });
 
   it('there is a star icon on favorite Pokémon', () => {
