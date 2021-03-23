1e352544c658e862fabd49ffdc3e41dcc1781415
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-03-23 10:04:23 -0300

    Feat: Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { screen } from '@testing-library/dom';
 import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
+import { NotFound } from '../components';
 
 describe('NotFound.js', () => {
   test('Teste se pág contém 1 heading h2 com o texto Page requested not found 😭', () => {
@@ -18,5 +19,14 @@ describe('NotFound.js', () => {
 
     const text = screen.getByText('Page requested not found');
     expect(text).toBeInTheDocument();
+  });
+  test('Teste se pág mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const NotFoundImage = getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(NotFoundImage).toBeInTheDocument();
+    expect(NotFoundImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
