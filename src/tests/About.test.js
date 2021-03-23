d7625f750e7f9f14b0124665f1dd8b28f4427da4
Perycles floriano <perycles_reis@hotamil.com>
2021-03-22 23:40:35 -0300

    requisito 01 refactoring

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWhithRouter from '../components/RenderWithRouter';
+import { About } from '../components';
+
+test('Teste se a página contém as informações sobre a Pokédex.',
+  () => {
+    renderWhithRouter(<About />);
+    const paragraph = screen.getByText();
+  });
+test('Teste se a página contém um heading h2 com o texto About Pokédex.',
+  () => {});
+test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.',
+  () => {});
+test('Teste se a página contém a seguinte imagem de uma Pokédex.',
+  () => {
+    const img = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  });
