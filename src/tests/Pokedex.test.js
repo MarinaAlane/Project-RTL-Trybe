4f7878c6e96152679419e1c5d312f22966125a4d
RodrigoNeoEx <draja.acc@gmail.com>
2021-03-23 14:01:25 -0300

    changed forEach on last test, now striker ok

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -49,18 +49,19 @@ test(
     pokemons.forEach(({ type }) => {
       expect(getByRole('button', { name: type })).toBeInTheDocument();
     });
+    expect(getByRole('button', { name: 'All' })).toBeInTheDocument();
   },
 );
 
-// test(
-//   'O botão de Próximo pokémon deve ser desabilitado quando tiver um só pokémon',
-//   () => {
-//     const { getByRole } = renderWithRouter(<App />);
-//     const btn = getByRole('button', { name: 'Próximo pokémon' });
-//     pokemons.forEach((type) => {
-//       if (type.type === pokemons.type) {
-//         expect(btn).not.toBeDisabled();
-//       } else expect(btn).toBeDisabled();
-//     });
-//   },
-// );
+test(
+  'O botão de Próximo pokémon deve ser desabilitado quando tiver um só pokémon',
+  () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const btn = getByRole('button', { name: 'Próximo pokémon' });
+    pokemons.forEach(({ type }) => {
+      if (type.type === pokemons.type) {
+        expect(btn).not.toBeDisabled();
+      } else expect(btn).toBeDisabled();
+    });
+  },
+);
