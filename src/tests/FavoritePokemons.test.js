c95f6c979087800a97a2b7fc40d3bebd99db9a73
Uibirá <usbarreto@gmail.com>
2021-03-23 11:21:29 -0300

    Req.3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,13 @@
-// import React from 'react';
-// import FavoritePokemons from '../components/FavoritePokemons';
-// import { screen } from '@testing-library/react';
-// import renderWithRouter from '../renderWithRouter';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
 
-// test('Renders `No favorite pokemon found` if there are not favorite pokémons', () => {});
+test('Renders `No favorite pokemon found` if there are not favorite pokémons', () => {
+  const mockPokemons = [];
+  renderWithRouter(<FavoritePokemons pokemons={ mockPokemons } />);
+  const noFavoritePokemons = screen.getByText(/no favorite pokemon found/i);
+  expect(noFavoritePokemons).toBeInTheDocument();
+});
+
+// npx stryker run ./stryker/FavoritePokemons.conf.json
