5c19891b72404119af915d47009140689356eb48
Perycles floriano <perycles_reis@hotamil.com>
2021-03-24 11:55:24 -0300

    requisito NotFound mutation test completo v1.0

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { fireEvent, screen } from '@testing-library/react';
-import renderWhithRouter from '../components/RenderWithRouter';
+// import React from 'react';
+// import { fireEvent, screen } from '@testing-library/react';
+// import renderWhithRouter from '../components/RenderWithRouter';
 
 test('', () => {
 
