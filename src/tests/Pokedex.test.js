a67b9e24be6f99520b36864e6adb9417cc2b13b6
Micaela Alves <micaela.alves93@gmail.com>
2021-03-23 23:45:52 -0300

    Requisito 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,19 +1,3 @@
-import React from 'react';
-import renderWithRouter from '../renderWithRouter';
-import Pokedex from '../components/Pokedex';
-
 describe('Test the Pokedex.js component', () => {
-  test('the page contains an h2 heading', () => {
-    const { getByRole } = renderWithRouter(<Pokedex />);
-    expect(getByRole('heading', {
-      level: 2,
-      name: /Encountered pokémons/i,
-    })).toBeInTheDocument();
-  });
-
-  test('O botão deve conter o texto Próximo pokémon', () => {
-    const { getByRole } = renderWithRouter(<Pokedex />);
-    const nextPokemon = getByRole('button', { name: /Próximo pokémon/i });
-    expect(nextPokemon).toBeInTheDocument();
-  });
+  test('', () => { });
 });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,6 +1,3 @@
-/* import React from 'react';
-import PokemonDetails from '../components/PokemonDetails'; */
-
 describe('Test the PokemonDetails.js component', () => {
   test('', () => { });
 });
