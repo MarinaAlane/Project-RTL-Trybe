17259d6e0528c353fae9b994de0fd69cb4f06f38
Enio Nicael <enionicael@gmail.com>
2021-03-24 09:43:26 -0300

    Requisito 1 implementa teste para verificar se o link Home redireciona para /.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,28 +1,38 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 describe('Comportamentos do App.', () => {
-  test('Renderiza um cabecalho com o texto `Pokédex`', () => {
+  it('Renderiza um cabecalho com o texto `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
 
-    const { getByText } = render(<App />);
-    /* const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    ); */
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
 
-  test('Mostra a Pokédex quando o route for `/`', () => {
-    const { getByText } = render(
-      <MemoryRouter initialEntries={ ['/'] }>
-        <App />
-      </MemoryRouter>,
-    );
+  it('Mostra a Pokédex quando o route for `/`', () => {
+    const { getByText } = renderWithRouter(<App />);
 
     expect(getByText('Encountered pokémons')).toBeInTheDocument();
   });
+
+  it('Testa se App contem um conjunto de links de navegacao', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    expect(getByText(/Home/i)).toBeInTheDocument();
+    expect(getByText(/About/i)).toBeInTheDocument();
+    expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
+  });
+
+  test('Testa se a aplicação é redirecionada para a página inicial, na URL "/" ao clicar no link Home da barra de navegação', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    fireEvent.click(getByText(/Home/i));
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/');
+  });
+  // test('', () => {});
 });
