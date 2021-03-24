c4957e03b17614e89c2996dd4df74b5278e48153
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-03-24 19:36:24 -0300

    task2 - ESLINT

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import { render, fireEvent, cleanup } from '@testing-library/react';
-import App from '../App';
+import { cleanup } from '@testing-library/react';
 
 afterEach(cleanup);
 
 // arquivo App.test.js pode servir de exemplo
 describe('Teste da aplicação, testando o botão e sua funcionalidade', () => {
-  test('ttt', () => {
-  });
+  test('ttt', () => { });
 });
