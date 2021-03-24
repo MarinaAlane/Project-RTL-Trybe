d337e2587761fa9a09005e0eb85c53323e2e1d1c
Carine Becker <caarine.m@gmail.com>
2021-03-24 18:41:38 -0300

    requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -5,9 +5,7 @@ import renderWithRouter from './renderWithRouter';
 import Pokedex from '../components/Pokedex';
 import isPokemonFavoriteById from '../App';
 
-const moreDetails = 'More details';
-
-test('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
+test('É renderizado um card com as informações de determinado pokémon.', () => {
   const { getByTestId, getByAltText } = renderWithRouter(<Pokedex
     pokemons={ pokemons }
     isPokemonFavoriteById={ isPokemonFavoriteById }
@@ -21,13 +19,13 @@ test('Teste se é renderizado um card com as informações de determinado pokém
   expect(pokemonImg.src).toBeDefined();
 });
 
-test('Teste se o card do Pokémon contém um link de nav para exibir detalhes.', () => {
+test('O card do Pokémon contém um link de nav para exibir detalhes.', () => {
   const { getByRole } = renderWithRouter(<Pokedex
     pokemons={ pokemons }
     isPokemonFavoriteById={ isPokemonFavoriteById }
   />);
 
-  const moreDetailsLink = getByRole('link', { name: moreDetails });
+  const moreDetailsLink = getByRole('link', { name: 'More details' });
   expect(moreDetailsLink).toBeDefined();
 });
 
@@ -37,23 +35,17 @@ test('Ao clicar no link de navegação do Pokémon, é redirecionado para Detalh
     isPokemonFavoriteById={ isPokemonFavoriteById }
   />);
 
-  const moreDetailsLink = getByRole('link', { name: moreDetails });
+  const moreDetailsLink = getByRole('link', { name: 'More details' });
   userEvent.click(moreDetailsLink);
   const { pathname } = history.location;
   expect(pathname).toMatch(/pokemons\/\d{2}/i);
 });
 
 test('Existe um ícone de estrela nos Pokémons favoritados.', () => {
-  const { getByRole, queryByAltText, getByText } = renderWithRouter(<Pokedex
+  const { queryByAltText } = renderWithRouter(<Pokedex
     pokemons={ pokemons }
     isPokemonFavoriteById={ isPokemonFavoriteById }
   />);
 
   expect(queryByAltText(/\w\sis\smarked\sas\sfavorite/i)).toBeDefined();
-
-  const moreDetailsLink = getByText(moreDetails);
-  userEvent.click(moreDetailsLink);
-
-  const favoriteChecked = getByRole('checkbox', { checked: true });
-  expect(favoriteChecked).toBeDefined();
 });
