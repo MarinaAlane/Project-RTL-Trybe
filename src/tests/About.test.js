1b43ff27fa72c7a4373e6c43b0b20c9aaf35498f
Perycles floriano <perycles_reis@hotamil.com>
2021-03-24 11:18:17 -0300

    requisito About mutation test completo v1.0

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -36,6 +36,6 @@ test('Teste se a página contém a seguinte imagem de uma Pokédex.',
     const imageContens = screen.getByRole('img');
     expect(imageContens).toBeInTheDocument();
     expect(imageContens.src).toBe(
-      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+      'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
     );
   });
