2cef4eca00299652a631cb5576017a173b467105
Marina Alane <marina.alane@outlook.com>
2021-03-24 12:51:10 -0300

    requisito 4 part 1

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Componente NotFound.js', () => {
+  it('A página deve ter um heading h2 com o texto encountered pokemons',
+    () => {
+    // acessa o elemento
+      renderWithRouter(<NotFound />);
+      const notFoundText = screen.getByText('Page requested not found', {
+        level: 2,
+        name: /Page requested not found 😭/i,
+      });
+      // faça o teste
+      expect(notFoundText).toBeInTheDocument();
+    });
+});
