573012e5ba1ce90b4c40f4ed079855cc64f7ccca
Enio Nicael <enionicael@gmail.com>
2021-03-25 01:48:00 -0300

    Requisito 2 testa se a pagina contem uma determinada imagem.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -8,11 +8,27 @@ describe('2. Testa o componente <About.js />.', () => {
     const { getByRole } = renderWithRouter(<About />);
 
     const heading = getByRole('heading');
-    console.log(heading);
 
     expect(heading.nodeName).toBe('H2');
     expect(heading.innerHTML).toBe('About Pokédex');
     expect(heading).toBeInTheDocument();
   });
-  // it('Testa se a página contém um heading "h2".', () => {});
+
+  /* it('Testa se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const paragraphs = 
+  }); */
+
+  it('Testa se a página contém uma determinada imagem de uma Pokédex', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+
+    const pokedexImg = getByAltText('Pokédex');
+    console.log(pokedexImg.src);
+
+    const src = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(pokedexImg.src).toBe(src);
+    expect(pokedexImg).toBeInTheDocument();
+  });
 });
