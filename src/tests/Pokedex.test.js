8cdecbfdb47be14a731bd02529147fff8af189db
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-03-24 21:54:21 -0300

    More tests

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 describe('Tests Pokedex', () => {
@@ -16,5 +16,19 @@ describe('Tests Pokedex', () => {
   test('Tests Next Pokémon Button', () => {
     const nextButton = screen.getByTestId('next-pokemon');
     expect(nextButton).toHaveTextContent('Próximo pokémon');
+  });
+
+  test('Tests filter buttons', () => {
+    const buttonNames = [
+      'Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+    const filterButtons = screen.getAllByTestId('pokemon-type-button');
+    filterButtons.forEach((filterButton, index) => {
+      expect(filterButton).toHaveTextContent(buttonNames[index]);
+    });
+  });
+
+  test('Tests reset filters button', () => {
+    const resetFiltersButton = screen.getByText('All');
+    fireEvent(resetFiltersButton, 'click');
   });
 });
