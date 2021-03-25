3507471d5c8ad24c9cc4eb013913e30a926cc1b5
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-03-24 21:28:43 -0300

    task 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('testing App component paths of the webpage', () => {
+  it('Testing NotFound page', () => {
+    const { getByRole } = render(<NotFound />);
+    const notFound = getByRole('heading', { text: 'Page requested' });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('testing if it has an especific image gif', () => {
+    const { getByAltText } = render(<NotFound />);
+    const imagePath = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(img).toBeInTheDocument();
+    expect(img.src).toBe(imagePath);
+  });
+});
