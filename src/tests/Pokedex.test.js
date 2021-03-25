e9f9f4aeab99e44cbe53813a3c6dd64c28086281
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-03-24 21:32:15 -0300

    Let's query stryker output

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Tests Pokedex', () => {
+  beforeEach(() => {
+    renderedScreen = render(<MemoryRouter><App /></MemoryRouter>);
+  });
+
+  test('Tests encountered Pokémons', () => {
+    const headings = screen.getByRole('heading', { level: 2 });
+    expect(headings).toHaveTextContent('Encountered pokémons');
+  });
+
+  test('Tests Next Pokémon Button', () => {
+    const nextButton = screen.getByTestId('next-pokemon');
+    expect(nextButton).toHaveTextContent('Próximo pokémon');
+  });
+});
