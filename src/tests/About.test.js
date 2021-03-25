8d2ec6be438250e6a5e945f28f8781be294dd357
jiarguello <jhonatan.arguello@gmail.com>
2021-03-25 00:06:21 -0300

    Iniciando o requisito 2.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testing About component', () => {
+  it('verify if the page have the informations of Pokédex', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const about = getByText(/about/i);
+
+    userEvent.click(about);
+  });
