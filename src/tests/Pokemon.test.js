b5554000bd4da236d646d3a41d96385024b0015e
Bruno <brunocmoraes97@gmail.com>
2021-03-25 18:01:01 -0300

    Inicio do requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, getByRole, getByTestId } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testing Pokemon.js', () => {
+  test('tests whether a card with pokemon information is rendered', () => {
+    const { getByTestId, getByAltText, getByRole } = renderWithRouter(<App />);
+    expect(getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
+    expect(getByTestId('pokemonType')).toHaveTextContent('Electric');
+    expect(getByTestId('pokemon-weight')).toHaveTextContent('Average weight: 6.0 kg');
+    expect(getByRole('img')).toBeInTheDocument();
+    expect(getByRole('img'.alt)).toBe(`${pokemons[0].name} sprite`);
+  });
+});
