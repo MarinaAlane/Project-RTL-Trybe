8002d0b72631e7c30dc5e575a98ca971f9e33d21
Enio Nicael <enionicael@gmail.com>
2021-03-25 19:35:49 -0300

    Requisito 3 testa se uma mensagem de nao encontrado e exibida caso nao tenha pokemons favoritos.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela uma mensagem se a pessoa nao tiver pokemons favoritos', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+    const notFound = getByText(/No favorite pokemon found/i);
+
+    expect(notFound).toBeInTheDocument();
+  });
+
+  // it('', () => {});
+  // it('', () => {});
+});
