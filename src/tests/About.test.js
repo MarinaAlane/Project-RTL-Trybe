17878e89567ffb636443d37bc5ca0ebf3fcc8afe
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-03-26 00:59:33 -0300

    Requisito 1 ok

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,11 @@
-test('', () => {
-  //VQV!! madrugar no projeto!!
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Test About Component', () => {
+  it('should render main page', () => {
+    const { getByText } = render(<About />);
+    const pageTitle = getByText('About Pokédex');
+    expect(pageTitle).toBeInTheDocument();
+  });
 });
