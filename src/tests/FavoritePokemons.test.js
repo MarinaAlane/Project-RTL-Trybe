54f169fd543e418b59f2a13de5f81063c35ed436
Enio Nicael <enionicael@gmail.com>
2021-03-26 11:44:29 -0300

    Requisito 3 implementa teste para verificar se todos os cards de pokémons favoritados sao exibidos.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -5,17 +5,25 @@ import renderWithRouter from './renderWithRouter';
 describe('Testa o componente <FavoritePokemons.js />', () => {
   it('Teste se mostra na tela uma mensagem se nao tiver pokemons favoritos', () => {
     const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const notFoundRegex = /No favorite pokemon found/i;
+    const h2 = getByText(notFoundRegex);
 
-    const notFound = getByText(/No favorite pokemon found/i);
-
-    expect(notFound).toBeInTheDocument();
+    expect(h2).toBeInTheDocument();
   });
 
-  /* it('Teste se é exibido todos os cards de pokémons favoritados', () => {
-    const { getByText, getAllByAltText } = renderWithRouter(<FavoritePokemons />);
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { queryAllByAltText, getByText } = renderWithRouter(<FavoritePokemons />);
 
-    const favorites = getAllByAltText(/favorite/i);
-    console.log(favorites);
-  }); */
+    const notFoundRegex = /No favorite pokemon found/i;
+    const h2 = getByText(notFoundRegex);
+    const notFound = notFoundRegex.test(h2.innerHTML);
+    console.log('not found', notFound);
+
+    if (!notFound) {
+      const favorites = queryAllByAltText(/is marked es favorite/i);
+      console.log(favorites);
+      expect(favorites).toBeDefined();
+    }
+  });
   // it('', () => {});
 });
