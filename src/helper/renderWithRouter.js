f10435392a83ad41d7833cf6ef5e138f6d7c4c4a
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-03-25 23:38:51 -0300

    req1: refactoring initial test as using helper function

--- src/helper/renderWithRouter.js
+++ src/helper/renderWithRouter.js
@@ -5,10 +5,10 @@ import { render } from '@testing-library/react';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
-  return {
+  return ({
     ...render(<Router history={ history }>{component}</Router>),
     history,
-  };
+  });
 };
 
 export default renderWithRouter;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../helper/renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
 test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-
+  const { getByText, history } = renderWithRouter(<App />);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
