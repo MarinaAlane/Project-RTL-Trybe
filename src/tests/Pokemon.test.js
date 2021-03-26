1ac55b2c34500c200be03901d4edbe45fdf5eaa1
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-03-26 08:57:00 -0300

    task6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -9,16 +9,22 @@ afterEach(cleanup);
 
 describe('testing Pokemon Component', () => {
   it('pokemons element has correct Name, type weight and image', () => {
-    const { getByText, getByTestId, getByAltText } = renderWithRouter(<App />);
-    const linkMDetails = getByText(/More Details/i);
-    const pokemonName = getByTestId('pokemon-name');
-    const dataName = data.find((pokemon) => (pokemon.name === pokemonName.innerHTML));
-    const { averageWeight: { value, measurementUnit }, name } = dataName;
+    const {
+      getByText,
+      getByTestId,
+      getByAltText,
+      getByLabelText,
+      history,
+    } = renderWithRouter(<App />);
+    const nextPokemon = getByText(/Próximo pokémon/i);
     const pokemonsClasses = 8;
     const numbuerOfClicks = Math.floor(Math.random() * pokemonsClasses) + 1;
     for (let i = 0; i <= numbuerOfClicks; i += 1) {
-      userEvent.click(linkMDetails);
+      userEvent.click(nextPokemon);
     }
+    const pokemonName = getByTestId('pokemon-name');
+    const dataName = data.find((pokemon) => (pokemon.name === pokemonName.innerHTML));
+    const { averageWeight: { value, measurementUnit }, name } = dataName;
     expect(pokemonName).toHaveTextContent(name);
     const pokemonWeight = getByTestId('pokemon-weight');
     const weightText = `Average weight: ${value} ${measurementUnit}`;
@@ -27,5 +33,18 @@ describe('testing Pokemon Component', () => {
     expect(pokemonType).toHaveTextContent(dataName.type);
     const pokemonImage = getByAltText(`${dataName.name} sprite`);
     expect(pokemonImage.src).toBe(dataName.image);
+    const linkMDetails = getByText(/More Details/i);
+    expect(linkMDetails).toBeInTheDocument();
+    userEvent.click(linkMDetails);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe(`/pokemons/${dataName.id}`);
+    const pokemonFav = getByLabelText('Pokémon favoritado?');
+    userEvent.click(pokemonFav);
+    expect(pokemonFav).toBeChecked();
+    const FavLink = getByText('Favorite Pokémons');
+    userEvent.click(FavLink);
+    const pokemonPicture = getByAltText(`${name} is marked as favorite`);
+    expect(pokemonPicture).toBeInTheDocument();
+    expect(pokemonPicture.src).toMatch(/star-icon.svg/i);
   });
 });
