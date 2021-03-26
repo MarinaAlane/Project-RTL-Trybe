2738b64b185a3c56f92d150647985e2afbf5ae29
jiarguello <jhonatan.arguello@gmail.com>
2021-03-26 13:58:49 -0300

    Pausa para o inicio do dia.

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testing Pokémon Details component', () => {
+  it('verify with all informantion are render on the screen', () => {
+    renderWithRouter(<App />);
+  });
+});
