94603a7f130f6819080d33126d3041fa8a0b1123
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-03-26 12:08:34 -0300

    Corrections Requirement 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,6 +4,9 @@ import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 import pokemons from '../data';
 
+const moreDetailsBtnName = 'More details';
+const pokemonNameTestId = 'pokemon-name';
+
 describe('Test the component Pokemon', () => {
   test('Test if Pokemon name is correct', () => {
     const { getAllByTestId, getByRole, getByTestId } = renderWithRouter(<App />);
@@ -22,7 +25,7 @@ describe('Test the component Pokemon', () => {
       const typeBtn = getByRole('button', { name: type });
       fireEvent.click(typeBtn);
 
-      const pokemonName = getByTestId('pokemon-name').innerHTML;
+      const pokemonName = getByTestId(pokemonNameTestId).innerHTML;
       const pokemonType = getByTestId('pokemonType').innerHTML;
       const pokemonWeight = getByTestId('pokemon-weight').innerHTML;
       const pokemonImage = getByRole('img', { name: `${pokemonName} sprite` });
@@ -36,17 +39,32 @@ describe('Test the component Pokemon', () => {
 
   test('Test on the Pokemon card, if there is a link to see more datails', () => {
     const { getByRole, getByTestId } = renderWithRouter(<App />);
-    const moreDetailsLink = getByRole('link', { name: 'More details' });
-    const pokemonName = getByTestId('pokemon-name').innerHTML;
+    const moreDetailsLink = getByRole('link', { name: moreDetailsBtnName });
+    const pokemonName = getByTestId(pokemonNameTestId).innerHTML;
     const pokemonId = pokemons.filter((pokemon) => pokemon.name === pokemonName)[0].id;
     expect(moreDetailsLink).toHaveProperty('href', `http://localhost/pokemons/${pokemonId}`);
   });
 
   test('Test if more details link redirect to details page', () => {
     const { getByRole } = renderWithRouter(<App />);
-    const moreDetailsLink = getByRole('link', { name: 'More details' });
+    const moreDetailsLink = getByRole('link', { name: moreDetailsBtnName });
     fireEvent.click(moreDetailsLink);
     const detailsText = getByRole('heading', { name: /Details/ });
     expect(detailsText).toBeInTheDocument();
+  });
+
+  test('Test if there is a favorite icon in pokemon favorited', () => {
+    const { getByRole, getByText, getByTestId } = renderWithRouter(<App />);
+    const moreDetailsLink = getByRole('link', { name: moreDetailsBtnName });
+    fireEvent.click(moreDetailsLink);
+    const favoriteSelector = getByText('Pokémon favoritado?');
+    fireEvent.click(favoriteSelector);
+    const pokemonName = getByTestId(pokemonNameTestId).innerHTML;
+    const favoriteIcon = getByRole(
+      'img',
+      { name: `${pokemonName} is marked as favorite` },
+    );
+    expect(favoriteIcon).toBeInTheDocument();
+    expect(favoriteIcon.src).toBe('http://localhost/star-icon.svg');
   });
 });
