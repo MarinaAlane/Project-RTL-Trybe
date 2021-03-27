9748f5d7d27440d5c39cbf8e8eb7f4445e4064a0
Caio <caioqs.info@gmail.com>
2021-03-27 20:05:22 -0300

    Feitio requisito 5-Corrigio ESlint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
