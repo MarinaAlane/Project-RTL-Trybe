458e04c910c715b8e3512f8e7f0dabcdce7e74e3
Caio <caioqs.info@gmail.com>
2021-03-27 11:46:40 -0300

    Feitio requisito 2-corrigido test

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -16,9 +16,11 @@ describe('Testando componente About', () => {
   });
 
   it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
-    const { getAllByTestId } = renderWithRouter(<About />);
-    const paragraphers = getAllByTestId('about-info');
-    expect(paragraphers.length).toBe(2);
+    const { getByText } = renderWithRouter(<About />);
+    const paragrapher1 = getByText(/This application simulates a Pokédex/i);
+    const paragrapher2 = getByText(/One can filter Pokémons by type/i);
+    expect(paragrapher1).toBeInTheDocument();
+    expect(paragrapher2).toBeInTheDocument();
   });
 
   it('este se a página contém a seguinte imagem de uma Pokédex', () => {
