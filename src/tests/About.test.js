a1a4aaf3c8161a621529159e84cee944303020d8
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-03-26 22:48:09 -0300

    req2: <About /> component test 1 and 2 there are infos and heading

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+import { fireEvent } from '@testing-library/react';
+import About from '../components/About';
+
+test('there are Pokédex info', () => {
+  const { getByText } = renderWithRouter(<About />);
+  const pokedexInfoTop = getByText(
+    'This application simulates a Pokédex'
+    + ', a digital encyclopedia containing all Pokémons',
+  );
+  const pokedexInfoWhat = getByText(
+    'One can filter Pokémons by type, and see more details for each one of them',
+  );
+  expect(pokedexInfoTop).toBeDefined();
+  expect(pokedexInfoWhat).toBeDefined();
+});
+test('there are h2 heading with About Pokédex text', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const aboutPokedex = getByRole('heading', {level: 2, name: 'About Pokédex'});
+  expect(aboutPokedex).toBeDefined();
+});
