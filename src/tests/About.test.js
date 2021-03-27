4b969128ec05760364dbebb1abba46c846ff0446
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-03-27 00:11:42 -0300

    chore: fixing linter warnings

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import renderWithRouter from '../helper/renderWithRouter';
-import { fireEvent } from '@testing-library/react';
 import About from '../components/About';
 
 test('there are Pokédex info', () => {
@@ -17,14 +16,15 @@ test('there are Pokédex info', () => {
 });
 test('there are h2 heading with About Pokédex text', () => {
   const { getByRole } = renderWithRouter(<About />);
-  const aboutPokedex = getByRole('heading', {level: 2, name: 'About Pokédex'});
+  const aboutPokedex = getByRole('heading', { level: 2, name: 'About Pokédex' });
   expect(aboutPokedex).toBeDefined();
 });
 test('there are 2 paragraphs with Pokémon includes in their text', () => {
   const { getAllByText } = renderWithRouter(<About />);
-  const aboutParagraphs = getAllByText((content, element) => {
-    return element.tagName === 'P' && content.includes('Pokémons');
-  });
+  const aboutParagraphs = getAllByText((content, element) => (
+    element.tagName === 'P'
+    && content.includes('Pokémons')
+  ));
   expect(aboutParagraphs).toBeDefined();
   expect(aboutParagraphs.length).toBe(2);
 });
