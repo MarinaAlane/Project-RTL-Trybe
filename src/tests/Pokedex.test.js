842d24b8c1039fcaf9ff9ce15d591e1ef3291bfb
fomes <contato.felipeluna@gmail.com>
2021-03-28 12:33:09 -0300

    pokedex fix 12.33

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { fireEvent, cleanup } from '@testing-library/react';
 import App from '../App';
-import pokemons from '../data';
 import renderWithRouter from '../renderWithRouter';
 
 afterEach(cleanup);
