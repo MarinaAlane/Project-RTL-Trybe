a14846bb92e4cc7ffb1185dc147a28273f907af1
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-03-29 02:34:51 -0300

    req5: 'dynamic buttons were created sucessfull for each pokemon type'

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -5,10 +5,10 @@ import renderWithRouter from '../helper/renderWithRouter';
 import Pokedex from '../components/Pokedex';
 import { pokemonMock, isFavoritePokemonMock } from './__mocks__/pokemonMock';
 
-const POKEMON_NAME = 'pokemon-name';
-
 function renderPokedexSetup(props) {
-  const NUMBER_OF_BUTTONS = 7;
+  const NUMBER_OF_TYPE_BUTTONS = 8;
+  const ALL = 'All';
+
   const utils = renderWithRouter(
     <Pokedex
       pokemons={ pokemonMock }
@@ -16,19 +16,66 @@ function renderPokedexSetup(props) {
       { ...props }
     />,
   );
-  const nextPokemonButton = screen.getByRole('button', { name: /Próximo pokémon/i });
+  const nextPokemonButton = screen.getByRole('button', {
+    name: /Próximo pokémon/i,
+  });
   const pokemonTypesButtons = screen.queryAllByTestId('pokemon-type-button');
+  const buttonAll = screen.queryByRole('button', { name: ALL });
+  pokemonTypesButtons.push(buttonAll);
   expect(nextPokemonButton).toBeInTheDocument();
-  expect(pokemonTypesButtons).toHaveLength(NUMBER_OF_BUTTONS);
+  expect(pokemonTypesButtons).toHaveLength(NUMBER_OF_TYPE_BUTTONS);
 
   return {
     ...utils,
     nextPokemonButton,
     pokemonTypesButtons,
-    dbPokemonFetch: (data) => pokemonMock
-      .map((pokemon) => pokemon[data]),
-    dbPokemonFetchByType: (type) => pokemonMock
-      .filter((pokemon) => pokemon.type === type),
+    buttonAll,
+    dbPokemonFetch: (data) => pokemonMock.map((pokemon) => pokemon[data]),
+    dbPokemonFetchByType: (type) => pokemonMock.filter((pokemon) => {
+      if (type === ALL) return true;
+      return pokemon.type === type;
+    }),
+  };
+}
+
+function renderPokedexSetupWithChecks(props = {}) {
+  const utils = renderPokedexSetup(props);
+  const {
+    dbPokemonFetch,
+    dbPokemonFetchByType,
+    nextPokemonButton,
+    pokemonTypesButtons,
+  } = utils;
+  const pokemonName = screen.queryByTestId('pokemon-name');
+
+  const checkEachPokemonByNames = (pokemonNames = dbPokemonFetch('name')) => {
+    const { disabled: noTestAnotherPokemon } = nextPokemonButton;
+    pokemonNames.forEach((name) => {
+      expect(pokemonName).toHaveTextContent(name);
+      if (!noTestAnotherPokemon) {
+        userEvent.click(nextPokemonButton);
+      }
+    });
+    expect(pokemonName).toHaveTextContent(pokemonNames[0]);
+  };
+
+  const ALL = 'All';
+  const clickFilterAndVerifyChange = (type = ALL) => {
+    const filteredTestPokemons = dbPokemonFetchByType(type);
+    const filteredPokemonNames = filteredTestPokemons.map(
+      (pokemon) => pokemon.name,
+    );
+    const TEST_BUTTON = pokemonTypesButtons.find(
+      (typeButton) => typeButton.textContent === type,
+    );
+    userEvent.click(TEST_BUTTON);
+    checkEachPokemonByNames(filteredPokemonNames);
+  };
+
+  return {
+    ...utils,
+    checkEachPokemonByNames,
+    clickFilterAndVerifyChange,
   };
 }
 
@@ -43,90 +90,54 @@ test('there are h2 heading text: Encountered pokémons', () => {
   expect(heading.textContent).toBe('Encountered pokémons');
 });
 
-test('if next pokémon appears when button was pressed and loop over pokemons', () => {
-  const { dbPokemonFetch, nextPokemonButton } = renderPokedexSetup();
-
-  dbPokemonFetch('name').forEach((pokemon) => {
-    expect(screen.queryByTestId(POKEMON_NAME, { name: pokemon }))
-      .toHaveTextContent(pokemon);
-    userEvent.click(nextPokemonButton);
-  });
-
-  expect(screen.queryByTestId(POKEMON_NAME, { name: 'Pikachu' }))
-    .toHaveTextContent('Pikachu');
+test('next pokémon appears when next button was pressed and loop over pokemons', () => {
+  const { checkEachPokemonByNames } = renderPokedexSetupWithChecks();
+  checkEachPokemonByNames();
 });
 
 test('view only one pokemonCard when pokemonTypes/nextPokemon button is clicked', () => {
-  const { nextPokemonButton, pokemonTypesButtons } = renderPokedexSetup();
+  const {
+    pokemonTypesButtons,
+    clickFilterAndVerifyChange,
+  } = renderPokedexSetupWithChecks();
 
   pokemonTypesButtons.forEach((typeButton) => {
-    const { disabled: testAnotherPokemon } = nextPokemonButton;
-    expect(screen.queryAllByTestId(POKEMON_NAME)).toHaveLength(1);
-    if (!testAnotherPokemon) {
-      userEvent.click(nextPokemonButton);
-      expect(screen.queryAllByTestId(POKEMON_NAME)).toHaveLength(1);
-    }
-    userEvent.click(typeButton);
+    clickFilterAndVerifyChange(typeButton.textContent);
+    expect(screen.queryAllByTestId('pokemon-name')).toHaveLength(1);
   });
 });
 
 test('there are a filter fire button that only display filtered psychic pokemons', () => {
-  const POKEMON_TYPE = 'Psychic';
-  const FIRST_POKEMON = 'Alakazam';
-
-  const {
-    dbPokemonFetchByType,
-    nextPokemonButton,
-  } = renderPokedexSetup();
-
-  const pokemonByType = dbPokemonFetchByType(POKEMON_TYPE);
-  const pokemonTypeBtn = screen.queryByRole('button', { name: POKEMON_TYPE });
-  expect(pokemonTypeBtn).not.toBeNull();
-
-  userEvent.click(pokemonTypeBtn);
-
-  pokemonByType.forEach(({ name: pokeName }) => {
-    expect(screen.queryByTestId(POKEMON_NAME, { name: pokeName }))
-      .toHaveTextContent(pokeName);
-    userEvent.click(nextPokemonButton);
-  });
-  expect(screen.queryByTestId(POKEMON_NAME, { name: FIRST_POKEMON }))
-    .toHaveTextContent(FIRST_POKEMON);
+  const { clickFilterAndVerifyChange } = renderPokedexSetupWithChecks();
+  clickFilterAndVerifyChange('Psychic');
 });
 
-test.only('start with and there are All button to override any filtered view', () => {
-  const FILTER_TEST = 'Fire';
-
+test('start with and there are All button to override any filtered view', () => {
   const {
-    dbPokemonFetch: dbAllPokemonFetch,
-    dbPokemonFetchByType,
-    nextPokemonButton,
-    pokemonTypesButtons,
-  } = renderPokedexSetup();
+    checkEachPokemonByNames,
+    clickFilterAndVerifyChange,
+    buttonAll,
+  } = renderPokedexSetupWithChecks();
 
-  const allPokemonNames = dbAllPokemonFetch('name');
-  const filteredTestPokemons = dbPokemonFetchByType(FILTER_TEST);
-  const againButtonAll = screen.queryByRole('button', { name: /all/i });
-  const pokemonName = screen.queryByTestId(POKEMON_NAME);
-
-  const checkByPokemons = (pokemonNames = allPokemonNames) => {
-    pokemonNames.forEach((name) => {
-      expect(pokemonName).toHaveTextContent(name);
-      userEvent.click(nextPokemonButton);
-    });
-    expect(pokemonName).toHaveTextContent(pokemonNames[0]);
-  };
-  const clickFilterAndVerifyChange = (type = FILTER_TEST) => {
-    const TEST_BUTTON = pokemonTypesButtons.find(
-      (typeButton) => typeButton.textContent === type,
-    );
-    userEvent.click(TEST_BUTTON);
-    const filteredPokemonNames = filteredTestPokemons.map((pokemon) => pokemon.name);
-    checkByPokemons(filteredPokemonNames);
-  };
-
-  checkByPokemons();
+  checkEachPokemonByNames();
   clickFilterAndVerifyChange();
-  userEvent.click(againButtonAll);
-  checkByPokemons();
+
+  userEvent.click(buttonAll);
+
+  checkEachPokemonByNames();
+});
+
+test('dynamic buttons were created sucessfull for each pokemon type', () => {
+  const { pokemonTypesButtons, dbPokemonFetch, buttonAll } = renderPokedexSetup();
+
+  const typesNameButtonsWithOutAll = pokemonTypesButtons
+    .map((pokemonTypeButton) => pokemonTypeButton.textContent)
+    .filter((pokemonTypeButtonName) => pokemonTypeButtonName !== 'All');
+
+  const pokemonsByTypes = [...new Set(dbPokemonFetch('type'))];
+
+  pokemonsByTypes.forEach((type) => {
+    expect(typesNameButtonsWithOutAll).toContain(type);
+    expect(buttonAll).toBeInTheDocument();
+  });
 });
