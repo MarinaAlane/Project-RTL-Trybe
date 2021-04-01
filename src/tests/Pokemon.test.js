52a5c3438a33dbc52bea40cba9f01fd8c1f583e3
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-04-01 18:24:32 -0300

    Intitial Pokemon test setup

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+describe('Tests Pokemon', () => {
+  
+});
