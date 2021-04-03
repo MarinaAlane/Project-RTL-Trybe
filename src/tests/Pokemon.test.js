6d7b06f8ac26b297cca9c46e2da8ecd0765c0e65
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-04-03 13:13:45 -0300

    Remove globals

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import { createMemoryHistory } from 'history';
 import Pokemon from '../components/Pokemon';
