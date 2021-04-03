c0c9f2a73545ee48b395cf6ab13f5dcf47ce933f
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-04-03 11:32:31 -0300

    Tests Pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,10 +1,9 @@
-/*
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
+import { createMemoryHistory } from 'history';
 import Pokemon from '../components/Pokemon';
 import pokemons from '../data';
-*/
 
 const dummyArray = Array;
 dummyArray.prototype.randomElement = function randomElement() {
@@ -12,7 +11,28 @@ dummyArray.prototype.randomElement = function randomElement() {
 };
 
 describe('Tests Pokemon', () => {
+  const pokemon = pokemons.randomElement();
+  const isFavorite = true;
+  const showDetailsLink = true;
   test('', () => {
-    expect(1).toBe(1);
+    const history = createMemoryHistory();
+    render(
+      <MemoryRouter history={ history }>
+        <Pokemon
+          isFavorite={ isFavorite }
+          pokemon={ pokemon }
+          showDetailsLink={ showDetailsLink }
+        />
+      </MemoryRouter>,
+    );
+    expect(screen.getByTestId('pokemon-name')).toHaveTextContent(`${pokemon.name}`);
+    expect(screen.getByTestId('pokemonType')).toHaveTextContent(`${pokemon.type}`);
+    const { value, measurementUnit } = pokemon.averageWeight;
+    expect(screen.getByTestId('pokemon-weight'))
+      .toHaveTextContent(`Average weight: ${value} ${measurementUnit}`);
+    expect(screen.getByText('More details'))
+      .toHaveAttribute('href', `/pokemons/${pokemon.id}`);
+    expect(screen.getByAltText(`${pokemon.name} sprite`))
+      .toHaveAttribute('src', `${pokemon.image}`);
   });
 });
