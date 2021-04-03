f35ce415f462925d8de62d691b862819c704c2f7
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-04-03 09:29:18 -0300

    Mock setup

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,6 +4,16 @@ import { MemoryRouter } from 'react-router';
 import Pokemon from '../components/Pokemon';
 import pokemons from '../data';
 
+const dummyArray = Array;
+dummyArray.prototype.randomElement = function randomElement() {
+  return (this.length) ? this[Math.floor(Math.random() * this.length)] : undefined;
+};
+
 describe('Tests Pokemon', () => {
-  
+  const HALF = 0.5;
+  const isPokemonFavoriteById = pokemons.reduce((acc, pokemon) => {
+    acc[pokemon.id.toString()] = Math.random() < HALF;
+    return acc;
+  }, {});
+  const thisPokemon = pokemons.randomElement();
 });
