fb3a2485fee24e7710a6a9ce5edb23d1e511785c
BBrolesi <brunohsbrolesi@gmail.com>
2021-04-13 19:22:05 -0300

    push to sync

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Test \'Pokedex.js\' Component - Requirement 05', () => {
+  it('has \'h2\' with \'Encountered pokémons\' text', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const heading = getByRole('heading', { level: 2 });
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/Encountered pokémons/i);
+  });
+  it('Changes pokemons after click in \'Próximo pokémon\' button', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const nameTestId = 'pokemon-name';
+    const button = getByTestId('next-pokemon');
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('Próximo pokémon');
+    const firstPokemon = getByTestId(nameTestId).textContent;
+    userEvent.click(button);
+    const secondPokemon = getByTestId(nameTestId).textContent;
+    expect(firstPokemon).not.toBe(secondPokemon);
+    let pokemon;
+    const maxClicks = 100;
+    let loopComplete = false;
+    for (let i = 0; pokemon !== firstPokemon && i < maxClicks; i += 1) {
+      userEvent.click(button);
+      pokemon = getByTestId(nameTestId).textContent;
+      if (pokemon === firstPokemon) {
+        loopComplete = true;
+      }
+    }
+    expect(loopComplete).toBe(true);
+  });
+  it('Only have one pokémon at a time', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const pokémons = getAllByTestId('pokemon-name');
+    expect(pokémons.length).toBe(1);
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,68 @@
-test('', () => {});
+import React from 'react';
+import { cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import Pokemon from '../components/Pokemon';
+
+const dummyPokemon = {
+  id: 65,
+  name: 'Alakazam',
+  type: 'Psychic',
+  averageWeight: {
+    value: '48.0',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Alakazam_(Pok%C3%A9mon)',
+  foundAt: [
+    {
+      location: 'Unova Accumula Town',
+      map: 'https://cdn.bulbagarden.net/upload/4/44/Unova_Accumula_Town_Map.png',
+    },
+  ],
+  summary: 'Closing both its eyes heightens all its other senses',
+};
+
+describe('Test \'Pokemon.js\' Component - Requirement 06', () => {
+  it('renders a pokemon card with correct parameters', () => {
+    const { getByTestId, getAllByAltText } = renderWithRouter(
+      <Pokemon
+        pokemon={ dummyPokemon }
+        isFavorite={ false }
+      />,
+    );
+    const { name, type, averageWeight: { value, measurementUnit }, image } = dummyPokemon;
+    expect(getByTestId('pokemon-name').innerHTML).toBe(name);
+    expect(getByTestId('pokemonType').innerHTML).toBe(type);
+    expect(getByTestId('pokemon-weight').innerHTML)
+      .toBe(`Average weight: ${value} ${measurementUnit}`);
+    expect(getAllByAltText(`${name} sprite`)[0]).toHaveAttribute('src', image);
+  });
+  it('has navigation link to pokemon datails', () => {
+    const { queryByText } = renderWithRouter(
+      <Pokemon
+        pokemon={ dummyPokemon }
+        isFavorite={ false }
+      />,
+    );
+    const { id } = dummyPokemon;
+    const details = queryByText('More details');
+    expect(details).toBeInTheDocument();
+    expect(details).toHaveAttribute('href', `/pokemons/${id}`);
+  });
+  it('redirects to pokemon details', () => {
+    const { getByTestId, queryByText, getByRole } = renderWithRouter(<App />);
+    const name = getByTestId('pokemon-name').innerHTML;
+    const details = queryByText('More details');
+    userEvent.click(details);
+    const detailsHeader = getByRole(
+      'heading',
+      {
+        level: 2,
+        name: `${name} Details`,
+      },
+    );
+    expect(detailsHeader).toBeInTheDocument();
+  });
+});
