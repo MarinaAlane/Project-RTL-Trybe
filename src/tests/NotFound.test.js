1ae9c510a8bd00d6338cc60fb51e379504dd4773
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-04-18 22:20:22 -0300

    Quarto Requisito NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import App from '../App';
+import NotFound from '../components/NotFound';
 import renderWithRouter from '../services/renderWithRouter';
 
 describe('Testes do modulo NotFound', () => {
   it('Testa de a página tem o texto "Not Found"', () => {
-    const { getByText, history } = renderWithRouter(<App />);
-    history.push('/linkerrado');
+    const { getByText } = renderWithRouter(<NotFound />);
     const errorMessage = getByText(/Page requested not found/i);
     expect(errorMessage).toBeInTheDocument();
   });
 
   it('Testa se a imagem aparece', () => {
-    const { getByRole, history } = renderWithRouter(<App />);
-    history.push('/linkerrado');
+    const { getByRole } = renderWithRouter(<NotFound />);
     const image = getByRole('img');
     expect(image).toBeInTheDocument();
     expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
