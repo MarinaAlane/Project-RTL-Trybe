4b84f0cc8269d14ac2a58d8ef313ba22af4a5130
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-04-18 22:03:06 -0300

    Quarto Requisito NotFound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,10 +5,11 @@ const NotFound = () => (
   <div className="not-found">
     <h2>
       {`Page requested ` + `not found`}
-      <span role="img" aria-label="Crying emoji"> 😭</span>
+      <span aria-label="Crying emoji"> 😭</span>
     </h2>
     <img
       className="not-found-image"
+      role="img"
       src={`https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`}
       alt="Pikachu crying because the page requested was not found"
     />
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testes do modulo NotFound', () => {
+  it('Testa de a página tem o texto "Not Found"', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/linkerrado');
+    const errorMessage = getByText(/Page requested not found/i);
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it('Testa se a imagem aparece', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    history.push('/linkerrado');
+    const image = getByRole('img', { src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif' });
+    expect(image).toBeInTheDocument();
+    //expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
