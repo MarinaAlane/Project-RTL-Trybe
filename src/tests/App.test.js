850bde2cc3cc1affe036f740715acdab4a65775f
Rodrigo Farias <deltadigo@gmail.com>
2021-04-20 18:10:56 -0300

    Requisito 1 - Testes 1 e 2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,31 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('Testa o carregamento da página principal ao acessar o caminho da URL /', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const title = getByText('Encountered pokémons');
+  expect(title).toBeInTheDocument();
+});
+
+test('Testa se o topo da aplicação possui um conjunto de links de navegação', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const homeLink = getByText('Home');
+  expect(homeLink).toBeInTheDocument();
+
+  const aboutLink = getByText('About');
+  expect(aboutLink).toBeInTheDocument();
+
+  const favoriteLink = getByText('Favorite Pokémons');
+  expect(favoriteLink).toBeInTheDocument();
+})
