8401a98819a271a560ecdc2c6313f676d0e7f9e5
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-04-20 10:43:18 -0300

    requisito 6, Fix

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -11,7 +11,7 @@ it('render pokemons card info ', () => {
   const { getByTestId, getByAltText } = renderWithRouter(<App />);
   const { averageWeight: { value, measurementUnit } } = pokemon;
   expect(getByTestId('pokemon-name').textContent).toBe(pokemon.name);
-  expect(getByTestId('pokemonType').textContent).toBe(pokemon.type);
+  expect(getByTestId('pokemon-type').textContent).toBe(pokemon.type);
   expect(getByTestId('pokemon-weight').textContent)
     .toBe(`Average weight: ${value} ${measurementUnit}`);
   expect(getByAltText(`${pokemon.name} sprite`).src).toBe(pokemon.image);
