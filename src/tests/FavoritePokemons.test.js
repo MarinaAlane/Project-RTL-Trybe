cb4a6882761f5c8450c659fa177cf69e56817ba4
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-04-20 16:01:49 -0400

    req 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { FavoritePokemons } from '../components';
+
+describe('testing Favorite Pokemons', () => {
+  it('should apears No favorite pokemon found', () => {
+    render(
+      <BrowserRouter>
+        <FavoritePokemons />
+        ,
+      </BrowserRouter>,
+    );
+
+    const favorites = screen.queryAllByRole('p', /found/i);
+
+    console.log(favorites);
+  });
+});
