b5e9c08ee49c89dc593821293b1f0760dac6177c
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-04-20 01:19:08 -0300

    requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, getByAltText } from '@testing-library/react';
 import renderWithRouter from './History';
 import App from '../App';
 import pokemons from '../data';
@@ -16,8 +16,29 @@ it('render pokemons card info ', () => {
   expect(getByAltText(`${pokemon.name} sprite`).src).toBe(pokemon.image);
 });
 
-it('', () => {});
+it('Exist a link to show more info', () => {
+  const{ getByText } = renderWithRouter(<App />);
 
-it('', () => {});
+  expect(getByText(linkDetails)).toBeInTheDocument();
+});
 
-it('', () => {});
+it('More Details clicked redirect to Pokemon details', () => {
+  const { getByText } = renderWithRouter(<App />);
+  fireEvent.click(getByText(linkDetails));
+  expect(getByText('Summary')).toBeInTheDocument();
+});
+
+it('Change URL to pokemon/<id>', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  fireEvent.click(getByText(linkDetails));
+  const { pathname } = history.location;
+  expect(pathname).toBe(`/pokemons/${pokemon.id}`);
+});
+
+it('Show Star to favotited pokemon', () => {
+  const { getByText, getByAltText } = renderWithRouter(<App />);
+  fireEvent.click(getByText(linkDetails));
+  fireEvent.click(getByText('Pokémon favoritado?'));
+  const img = getByAltText(`${pokemon.name} is marked as favorite`);
+  expect(img.src).toContain('/star-icon.svg');
+});
