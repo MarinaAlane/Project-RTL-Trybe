f5ce9bbe143f0ddded9a617fbaefc8c03cdef895
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-04-20 01:00:06 -0300

    requisito 6, 1/4

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './History';
+import App from '../App';
+import pokemons from '../data';
+
+const pokemon = pokemons[0];
+const linkDetails = 'More details';
+
+it('render pokemons card info ', () => {
+  const { getByTestId, getByAltText } = renderWithRouter(<App />);
+  const { averageWeight: { value, measurementUnit } } = pokemon;
+  expect(getByTestId('pokemon-name').textContent).toBe(pokemon.name);
+  expect(getByTestId('pokemonType').textContent).toBe(pokemon.type);
+  expect(getByTestId('pokemon-weight').textContent).toBe(`Average weight: ${value} ${measurementUnit}`);
+  expect(getByAltText(`${pokemon.name} sprite`).src).toBe(pokemon.image);
+});
+
+it('', () => {});
+
+it('', () => {});
+
+it('', () => {});
