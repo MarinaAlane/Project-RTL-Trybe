b22474eefb392f6dad7971d051029fe8f7da90ba
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-04-21 11:16:05 -0400

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,19 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
 import { FavoritePokemons } from '../components';
 
 describe('testing Favorite Pokemons', () => {
   it('should apears No favorite pokemon found', () => {
+    render(<FavoritePokemons />);
+
+    const noFavorite = screen.getByText('No favorite pokemon found');
+
+    expect(noFavorite.innerHTML).toBe('No favorite pokemon found');
+  });
+
+  it('shoud have one favorite pokémon', () => {
+    const history = createMemoryHistory();
     render(
-      <BrowserRouter>
-        <FavoritePokemons />
-        ,
-      </BrowserRouter>,
+      <Router history={ history }>
+        <App />
+      </Router>,
     );
 
-    const favorites = screen.queryAllByRole('p', /found/i);
+    const moreDetailsButton = screen.getByText('More details');
+    userEvent.click(moreDetailsButton);
+    const favoritePokemon = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(favoritePokemon);
 
-    console.log(favorites);
+    expect(favoritePokemon.checked).toBe(true);
   });
 });
