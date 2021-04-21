81b44d74e82e0ca4f32eeb75d02a02fd62307207
Enio Nicael <enionicael@gmail.com>
2021-04-21 18:38:48 -0300

    Requisito 7 testa se todos os locais do pokemon sao mostrados.

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -8,7 +8,7 @@ import pokemons from '../data';
 describe('Teste o componente <PokemonDetails.js />', () => {
 
   it('Testa se os detalhes do Pokémon selecionado são mostradas na tela', () => {
-    const { queryByText, getByText, getByRole, getByTestId } = renderWithRouter(<App />);
+    const { queryByText, getByText, getAllByRole, getByTestId } = renderWithRouter(<App />);
     // A página deve conter um texto <name> Details, onde <name> é o nome do Pokémon
     const currentPokemon = getByTestId(/pokemon-name/i).innerHTML;
     const moreDetailsLink = getByText(/More details/i);
@@ -23,26 +23,36 @@ describe('Teste o componente <PokemonDetails.js />', () => {
     const linkInMoreDetails = queryByText(/More details/i);
     expect(linkInMoreDetails).toBe(null);
     // A seção de detalhes deve conter um heading h2 com o texto Summary
-    const heading = getByRole('heading', { level: 2, name: 'Summary' });
-    expect(heading).toBeInTheDocument();
+    const heading = getAllByRole('heading', { level: 2 });
+
+    expect(heading[1]).toHaveTextContent('Summary');
     // A seção de detalhes deve conter um parágrafo com o resumo do Pokémon específico sendo visualizado
     //let summary = '';
     const summary = pokemons.find(pokemon => pokemon.name === currentPokemon).summary;
     const pokemonSummary = getByText(summary);
     expect(pokemonSummary).toBeInTheDocument();
-    console.log(summary);
   });
-/* 
-  describe('Teste se existe na página uma seção com os mapas contendo as localizações do pokémon', () => {
-    it('Na seção de detalhes deverá existir um heading h2 com o texto Game Locations of <name>; onde <name> é o nome do Pokémon exibido', () => {
 
-    });
+  it('Teste se existe na página uma seção com os mapas contendo as localizações do pokémon', () => {
+    //Deve existir h2 com o texto Game Locations of <name>
+      const { getAllByAltText, getByText, getAllByRole, getByTestId } = renderWithRouter(<App />);
 
-    it('Todas as localizações do Pokémon devem ser mostradas na seção de detalhes', () => {
+      const currentPokemon = getByTestId(/pokemon-name/i).innerHTML;
+      const moreDetailsLink = getByText(/More details/i);
+      userEvent.click(moreDetailsLink);
 
-    });
+      const heading = getAllByRole('heading', { level: 2 });
+      //heading.forEach((value) => console.log(value.innerHTML));
+      expect(heading[2]).toHaveTextContent(`Game Locations of ${currentPokemon}`);
 
-    it('Devem ser exibidos, o nome da localização e uma imagem do mapa em cada localização', () => {
+    // Todas as localizações do Pokémon devem ser mostradas na seção de detalhes
+    const location = pokemons.find(pokemon => pokemon.name === currentPokemon).foundAt;
+
+    const locationImg = getAllByAltText(`${currentPokemon} location`);
+
+    expect(locationImg.length).toBe(location.length);
+
+    /* it('Devem ser exibidos, o nome da localização e uma imagem do mapa em cada localização', () => {
 
     });
 
@@ -52,9 +62,9 @@ describe('Teste o componente <PokemonDetails.js />', () => {
 
     it('A imagem da localização deve ter um atributo alt com o texto <name> location, onde <name> é o nome do Pokémon', () => {
 
-    });
+    }); */
   });
-
+/*
   describe('Teste se o usuário pode favoritar um pokémon através da página de detalhes', () => {
     it('A página deve exibir um checkbox que permite favoritar o Pokémon', () => {
 
