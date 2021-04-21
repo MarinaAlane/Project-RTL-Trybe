ccdf75fa459707ede80ab249f091805344243cef
Rodrigo Farias <deltadigo@gmail.com>
2021-04-20 21:52:27 -0300

    Requisito 2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { MemoryRouter, Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render, fireEvent } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 const renderWithHistory = (component) => {
