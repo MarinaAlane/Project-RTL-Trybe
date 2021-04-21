d217e20bf112d373392a3c40ba651fc1fd64f969
Enio Nicael <enionicael@gmail.com>
2021-04-21 17:24:23 -0300

    Requisito 7, testa se os detalhes do pokemon sao mostrados na tela

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,23 +1,38 @@
-/* describe('Teste o componente <PokemonDetails.js />', () => {
+import React from 'react';
+import userEvent from '@testing-library/user-event';
 
-  describe('Testa se os detalhes do Pokémon selecionado são mostradas na tela', () => {
-    it('A página deve conter um texto <name> Details, onde <name> é o nome do Pokémon', () => {
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
 
-    });
+describe('Teste o componente <PokemonDetails.js />', () => {
 
-    it('Não deve existir o link de navegação para os detalhes do Pokémon selecionado', () => {
+  it('Testa se os detalhes do Pokémon selecionado são mostradas na tela', () => {
+    const { queryByText, getByText, getByRole, getByTestId } = renderWithRouter(<App />);
+    // A página deve conter um texto <name> Details, onde <name> é o nome do Pokémon
+    const currentPokemon = getByTestId(/pokemon-name/i).innerHTML;
+    const moreDetailsLink = getByText(/More details/i);
+    userEvent.click(moreDetailsLink);
+    const pokemonDetailsText = getByText(`${currentPokemon} Details`);
+    expect(pokemonDetailsText).toBeInTheDocument();
 
-    });
+    //console.log(`current: ${currentPokemon}, detail: ${pokemonDetailsText.innerHTML}`);
 
-    it('A seção de detalhes deve conter um heading h2 com o texto Summary', () => {
-
-    });
-
-    it('A seção de detalhes deve conter um parágrafo com o resumo do Pokémon específico sendo visualizado', () => {
-
-    });
+    // Não deve existir o link de navegação para os detalhes do Pokémon selecionado
+    // userEvent.click(moreDetailsLink);
+    const linkInMoreDetails = queryByText(/More details/i);
+    expect(linkInMoreDetails).toBe(null);
+    // A seção de detalhes deve conter um heading h2 com o texto Summary
+    const heading = getByRole('heading', { level: 2, name: 'Summary' });
+    expect(heading).toBeInTheDocument();
+    // A seção de detalhes deve conter um parágrafo com o resumo do Pokémon específico sendo visualizado
+    //let summary = '';
+    const summary = pokemons.find(pokemon => pokemon.name === currentPokemon).summary;
+    const pokemonSummary = getByText(summary);
+    expect(pokemonSummary).toBeInTheDocument();
+    console.log(summary);
   });
-
+/* 
   describe('Teste se existe na página uma seção com os mapas contendo as localizações do pokémon', () => {
     it('Na seção de detalhes deverá existir um heading h2 com o texto Game Locations of <name>; onde <name> é o nome do Pokémon exibido', () => {
 
@@ -52,6 +67,6 @@
     it('O label do checkbox deve conter o texto Pokémon favoritado?', () => {
 
     });
-  });
+  }); */
 });
- */
+
