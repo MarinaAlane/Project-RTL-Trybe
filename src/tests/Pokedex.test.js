e62066610cbdbd3a2457ed249d673434f0d5db22
Rodrigo Farias <deltadigo@gmail.com>
2021-04-25 11:06:07 -0300

    Requisito 5 - Teste 1

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+
+  return {
+    ...render(<Router history={ history }>{ component }</Router>), history,
+  };
+};
+
+test('Testa se a página possui um h2 com o texto Encountered pokémons', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const h2 = getByText('Encountered pokémons');
+  expect(h2).toBeInTheDocument();
+});
