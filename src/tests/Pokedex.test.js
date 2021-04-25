520558a0b2ea3fa2417853a4da326527468dade9
Rodrigo Farias <deltadigo@gmail.com>
2021-04-25 13:06:02 -0300

    Requisito 5 - Teste 2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import pokemons from '../data';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
@@ -12,8 +14,31 @@ const renderWithRouter = (component) => {
   };
 };
 
-test('Testa se a página possui um h2 com o texto Encountered pokémons', () => {
-  const { getByText } = renderWithRouter(<App />);
-  const h2 = getByText('Encountered pokémons');
-  expect(h2).toBeInTheDocument();
+describe('Requisito 5', () => {
+  it('Testa se a página possui um h2 com o texto Encountered pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const h2 = getByText('Encountered pokémons');
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Testa todos os pokemons da lista ao clicar no botão Próximo pokémon', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const buttonNext = getByTestId('next-pokemon');
+    expect(buttonNext).toHaveTextContent('Próximo pokémon');
+    pokemons.forEach((pokemon) => {
+      const pokemonName = getByTestId('pokemon-name');
+      expect(pokemonName).toHaveTextContent(pokemon.name);
+      if (pokemonName === 'Dragonair') {
+        userEvent.click(buttonNext);
+        const firstPokemon = getByTestId('pokemon-name');
+        expect(firstPokemon).toHaveTextContent(pokemon[0].name);
+        return;
+      }
+      userEvent.click(buttonNext);
+    });
+
+    /* userEvent.click(buttonNext);
+    const h2 = getByText('Encountered pokémons');
+    expect(h2).toBeInTheDocument(); */
+  });
 });
