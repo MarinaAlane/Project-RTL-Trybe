59ede7ce8683175ca0cc0850e5e98ad1fafcb607
negres <nataliaknegres@gmail.com>
2021-04-26 21:27:34 -0300

    refatorando codigo

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,73 +1,54 @@
 import React from 'react';
-import { MemoryRouter, Router } from 'react-router-dom';
-import { fireEvent, render } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import App from '../App';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
 
-test('testa se é renderizado um card com o nome do pokémon', () => {
-  const { getByTestId } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
+test('Testa se renderiza as informações do pokémon', () => {
+  const { getByTestId, getByRole } = renderWithRouter(<Pokemon
+    pokemon={ pokemons[0] }
+    isFavorite={ false }
+  />);
+  const name = getByTestId('pokemon-name');
+  const type = getByTestId('pokemonType');
+  const weight = getByTestId('pokemon-weight');
+  const image = getByRole('img');
+  expect(name).toHaveTextContent(pokemons[0].name);
+  expect(type).toHaveTextContent(pokemons[0].type);
+  expect(weight).toHaveTextContent(
+    `Average weight: ${pokemons[0]
+      .averageWeight.value} ${pokemons[0]
+      .averageWeight.measurementUnit}`,
   );
-  const heading = getByTestId(/pokemon-name/i);
-  expect(heading).toBeInTheDocument();
-  expect(heading.textContent).toBe('Pikachu');
+  expect(image.src).toBe(pokemons[0].image);
+  expect(image.alt).toBe(`${pokemons[0].name} sprite`);
 });
 
-test('testa se é renderizado um card com o tipo do pokémon', () => {
-  const { getByTestId } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByTestId(/pokemonType/i);
-  expect(heading).toBeInTheDocument();
-  expect(heading.textContent).toBe('Electric');
+test('Testa more details link', () => {
+  const { getByRole } = renderWithRouter(<Pokemon
+    pokemon={ pokemons[0] }
+    isFavorite={ false }
+  />);
+  const details = getByRole('link', { name: 'More details' });
+  expect(details.href).toBe(`http://localhost/pokemons/${pokemons[0].id}`);
 });
 
-test('testa se é renderizado um card com o peso do pokémon', () => {
-  const { getByTestId } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByTestId(/pokemon-weight/i);
-  expect(heading).toBeInTheDocument();
-  expect(heading.textContent).toBe('Average weight: 6.0 kg');
+test('Testa more details link ao ser clicado', () => {
+  const { getByRole, history } = renderWithRouter(<Pokemon
+    pokemon={ pokemons[0] }
+    isFavorite={ false }
+  />);
+  const details = getByRole('link', { name: 'More details' });
+  fireEvent.click(details);
+  expect(history.location.pathname).toBe(`/pokemons/${pokemons[0].id}`);
 });
 
-test('testa se é exibida a imagem do pokémon', () => {
-  const { getByAltText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const image = getByAltText('Pikachu sprite');
-  expect(image.src).toContain('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
-});
-
-test('testa se existe o link More details', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/More details/i);
-  expect(heading).toBeInTheDocument();
-});
-
-test('Verifica se existe o id do Pokémon ao clicar em More details', () => {
-  const history = createMemoryHistory();
-  const { getByText, getByLabelText, getByAltText } = render(
-    <Router history={ history }>
-      <App />
-    </Router>,
-  );
-  fireEvent.click(getByText(/More details/i));
-  const { pathname } = history.location;
-  expect(pathname).toBe('/pokemons/25');
-  fireEvent.click(getByLabelText(/Pokémon favoritado/i));
-  const image = getByAltText('Pikachu is marked as favorite');
-  expect(image.src).toContain('/star-icon.svg');
+test('Testa icon favorito pokemon', () => {
+  const { getAllByRole } = renderWithRouter(<Pokemon
+    pokemon={ pokemons[0] }
+    isFavorite
+  />);
+  const allIcons = getAllByRole('img');
+  expect(allIcons[1].src).toBe('http://localhost/star-icon.svg');
+  expect(allIcons[1].alt).toBe(`${pokemons[0].name} is marked as favorite`);
 });
