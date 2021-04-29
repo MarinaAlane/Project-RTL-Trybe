9aa8850f48b6b2e2920af5158ee16789753e2414
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-04-29 17:19:42 -0400

    aqui foi

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,28 +1,58 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
+import { fireEvent } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
+import pokemons from '../data';
 
 describe('Test Component Pokemon', () => {
-  it('should have the pokemon card', () => {
-    const { getByTestId, getByAltText, getByText, getByLabelText } = renderWithRouter(
-      <App />,
+  const favoritePokemonIds = {
+    4: false,
+    10: false,
+    23: false,
+    25: false,
+    65: false,
+    78: false,
+    143: false,
+    148: false,
+    151: false,
+  };
+
+  it('should contains a Pokemon Card', () => {
+    const { history,
+      getByTestId,
+      getByAltText,
+      getByRole,
+      getByLabelText,
+    } = renderWithRouter(
+      <App
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ favoritePokemonIds }
+      />,
     );
 
-    const pokemonName = getByTestId('pokemon-name');
-    expect(pokemonName.innerHTML).toBe('Pikachu');
-    const pokemonType = getByTestId('pokemonType');
-    expect(pokemonType.innerHTML).toBe('Electric');
-    const pokemonWeight = getByTestId('pokemon-weight');
-    expect(pokemonWeight.innerHTML).toBe('Average weight: 6.0 kg');
-    const pokemonImg = getByAltText('Pikachu sprite');
-    expect(pokemonImg.src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
-    const pokemonDetails = getByText('More details');
-    expect(pokemonDetails.attributes[0].value).toBe('/pokemons/25');
-    userEvent.click(pokemonDetails);
-    // expect(history.location.pathname).toBe('/pokemons/25');
+    const name = getByTestId('pokemon-name');
+    expect(name).toHaveTextContent('Pikachu');
+
+    const type = getByTestId('pokemon-type');
+    expect(type).toHaveTextContent('Electric');
+
+    const weigth = getByTestId('pokemon-weight');
+    expect(weigth).toHaveTextContent('Average weight: 6.0 kg');
+
+    const image = getByAltText('Pikachu sprite');
+    expect(image.src).toBe('https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+
+    const details = getByRole('link', { name: 'More details' });
+    expect(details.href).toBe('http://localhost/pokemons/25');
+
+    fireEvent.click(details);
+    console.log(details);
+    expect(history.location.pathname).toBe('/pokemons/25');
+
     const favorite = getByLabelText('Pokémon favoritado?');
     userEvent.click(favorite);
+
     const favoriteIcon = getByAltText('Pikachu is marked as favorite');
     expect(favoriteIcon.src).toBe('http://localhost/star-icon.svg');
   });
