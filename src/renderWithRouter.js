03e8694c52f1e494d7cda2a7bf718c3a51721d14
nato-re <alvares.renato@hotmail.com>
2021-04-29 17:07:44 -0300

    [fix] Pokemon.js id typo

---
+++ src/renderWithRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  const aux = {...render(<Router history={history}>{component}</Router>), history}
+  console.log(aux)
+  return ({
+    ...render(<Router history={history}>{component}</Router>), history,
+  });
+};
+
+export default renderWithRouter;
