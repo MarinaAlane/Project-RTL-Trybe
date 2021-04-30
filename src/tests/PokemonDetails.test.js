b32c75449006a97525c3eee40744d95968700268
Thiago da Silva Gomes <thiagosilvagomes@gmail.com>
2021-04-29 23:24:44 -0300

    Requisito 07

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -35,31 +35,65 @@ describe('Teste o componente <PokemonDetails.js />', () => {
       /This intelligent Pokémon roasts hard berries with electricity/i,
     );
   });
+});
 
-  describe('Teste a favoritação de um pokémon através da página de detalhes.', () => {
-    it('Verifica se há um checkbox que permite favoritar o pokemon', () => {
-      const { getByRole, getByText, getByAltText } = RenderWithRouter(<App />);
-      const moreDetails = getByText(md);
-      fireEvent.click(moreDetails);
-      const cb = getByRole('checkbox');
-      expect(cb).toBeInTheDocument();
-      fireEvent.click(cb);
-      const img = getByAltText(/Pikachu is marked as favorite/i);
-      expect(img.src).toBe('http://localhost/star-icon.svg');
-      fireEvent.click(cb);
-      expect(img).not.toBeInTheDocument();
-      fireEvent.click(cb);
-      expect(img.src).toBe('http://localhost/star-icon.svg');
-      fireEvent.click(cb);
-      expect(img).not.toBeInTheDocument();
-    });
+describe('Teste a favoritação de um pokémon através da página de detalhes.', () => {
+  it('Verifica se há um checkbox que permite favoritar o pokemon', () => {
+    const { getByRole, getByText, getByAltText } = RenderWithRouter(<App />);
+    const moreDetails = getByText(md);
+    fireEvent.click(moreDetails);
+    const cb = getByRole('checkbox');
+    expect(cb).toBeInTheDocument();
+    fireEvent.click(cb);
+    const img = getByAltText(/Pikachu is marked as favorite/i);
+    expect(img.src).toBe('http://localhost/star-icon.svg');
+    fireEvent.click(cb);
+    expect(img).not.toBeInTheDocument();
+    fireEvent.click(cb);
+    expect(img.src).toBe('http://localhost/star-icon.svg');
+    fireEvent.click(cb);
+    expect(img).not.toBeInTheDocument();
+  });
 
-    it('O label do checkbox deve conter o texto Pokémon favoritado?', () => {
-      const { getByText } = RenderWithRouter(<App />);
-      const moreDetails = getByText(md);
-      fireEvent.click(moreDetails);
-      const labelFav = getByText(/Pokémon favoritado?/i);
-      expect(labelFav).toBeInTheDocument();
+  it('O label do checkbox deve conter o texto Pokémon favoritado?', () => {
+    const { getByText } = RenderWithRouter(<App />);
+    const moreDetails = getByText(md);
+    fireEvent.click(moreDetails);
+    const labelFav = getByText(/Pokémon favoritado?/i);
+    expect(labelFav).toBeInTheDocument();
+  });
+
+  it('Verifica imagens no location', () => {
+    const {
+      queryByText,
+      getByText,
+      getByRole,
+      getAllByAltText,
+    } = RenderWithRouter(<App />);
+    const moreDetails = getByText(md);
+    fireEvent.click(moreDetails);
+    const gameLocations = getByRole('heading', {
+      level: 2,
+      name: 'Game Locations of Pikachu',
     });
+    expect(gameLocations).toBeInTheDocument();
+    const location1 = queryByText('Kanto Viridian Forest');
+    const location2 = queryByText('Kanto Power Plant');
+    const maps = getAllByAltText('Pikachu location');
+    expect(location1).toBeInTheDocument();
+    expect(location2).toBeInTheDocument();
+    expect(maps[0].src).toBe('https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(maps[1].src).toBe('https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+  });
+
+  it('Testa se tem h2 com o texto "Game Locations of <name>', () => {
+    const { getByText, getByRole } = RenderWithRouter(<App />);
+    const moreDetails = getByText(md);
+    fireEvent.click(moreDetails);
+    const location = getByRole('heading', {
+      level: 2,
+      name: 'Game Locations of Pikachu',
+    });
+    expect(location).toBeInTheDocument();
   });
 });
