7696eada4e6cedae34af67174ff8bb95096e9a65
hannibal1207 <mathausannibal@gmail.com>
2021-05-12 12:59:49 -0300

    correções lint

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,4 +1,3 @@
-import { getAllByAltText } from '@testing-library/dom';
 import React from 'react';
 import NotFound from '../components/NotFound';
 import renderWithRouter from '../renderWithRouter';
