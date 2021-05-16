d26a8c4861a5599c1301c1d72a7f31448dbe086c
jepaur5 <jepaur5@gmail.com>
2021-05-16 00:56:46 -0300

    jesus requisito5 linha74 troca pokemon-type

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -71,7 +71,7 @@ describe('5. Teste o componente <Pokedex.js />', () => {
     });
     userEvent.click(buttonAll);
 
-    const pokemonElectric = getByTestId('pokemonType');
+    const pokemonElectric = getByTestId('pokemon-type');
     expect(pokemonElectric.textContent).toBe('Electric');
   });
 
