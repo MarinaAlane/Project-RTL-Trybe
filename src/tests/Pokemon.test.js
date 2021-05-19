3e80e4b9cf10cd36df3e3ef99da867e8ae9904d7
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-05-19 12:35:41 -0300

    Fix requisito 06

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+
+import React from 'react';
+import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Pokemon', () => {
+  it('information shown by the card is correct', () => {
+    const { getByTestId, getByAltText, getByText, history } = renderWithRouter(<App />);
+    const pokemonName = getByTestId('pokemon-name');
+    const pokemonType = getByTestId('pokemon-type');
+    const pokemonWeight = getByTestId('pokemon-weight');
+    const image = getByAltText('Pikachu sprite');
+
+    expect(pokemonName).toHaveTextContent('Pikachu');
+    expect(pokemonType).toHaveTextContent('Electric');
+    expect(pokemonWeight).toHaveTextContent('Average weight: 6.0 kg');
+    expect(image).toBeInTheDocument();
+    expect(image).not.toHaveAttribute('src', '');
+
+    const moreDetails = getByText('More details');
+    expect(moreDetails).toHaveAttribute('href', '/pokemons/25');
+    fireEvent.click(moreDetails);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+    const favorite = getByText('Pokémon favoritado?');
+    fireEvent.click(favorite);
+    history.push('/');
+    const favoriteIcon = getByAltText('Pikachu is marked as favorite');
+    expect(favoriteIcon).toHaveAttribute('src', '/star-icon.svg');
+    expect(favoriteIcon).toHaveAttribute('alt', 'Pikachu is marked as favorite');
+  });
+});
